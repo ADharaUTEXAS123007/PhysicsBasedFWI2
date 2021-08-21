5419386c6e463071e0529fdcbd683de7517a8876
Arnab Dhara <adhara@IG-A03506.local>
2021-08-21 12:24:57 -0500

    removed old models

--- train2patches4ASPPAug.h
+++
@@ -1,2 +0,0 @@
-
-no_proxy=localhost python trainVal4d.py --dataroot /glb/data/eptr_am_2/Arnab/volumes/training_data/events/HorizonWindow/att_volumes/patches4/ --name patches4ASPP --model ASPP --direction AtoB --input_nc 3 --output_nc 1 --display_port 9997 --n_epochs 1400 --n_epochs_decay 1400 --batch_size 128  --gpu_ids 0,1 --no_html --display_freq 40 --print_freq 40 --verbose
