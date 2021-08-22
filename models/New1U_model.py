893d96399343433e084ab330f344aec90cb642b1
Arnab Dhara <adhara@IG-A03506.local>
2021-08-22 14:40:09 -0500

    epoch1

--- models/New1U_model.py
+++ models/New1U_model.py
@@ -54,10 +54,12 @@ class New1UModel(BaseModel):
         #for i in range(3):
         torch.cuda.set_device(0)
         torch.cuda.set_device(2)
+        torch.cuda.set_device(1)
         #torch.cuda.set_device(2)
 
         self.device1 = torch.device('cuda:{}'.format(self.gpu_ids[0])) if self.gpu_ids else torch.device('cpu') 
         self.device2 = torch.device('cuda:{}'.format(self.gpu_ids[1])) if self.gpu_ids else torch.device('cpu') 
+        self.device3 = torch.device('cuda:{}'.format(self.gpu_ids[2])) if self.gpu_ids else torch.device('cpu') 
         # specify the training losses you want to print out. The training/test scripts will call <BaseModel.get_current_losses>
         self.loss_names = ['D_MSE','M_MSE','V_MSE']
         # specify the images you want to save/display. The training/test scripts will call <BaseModel.get_current_visuals>
--- trainVelModel.sh
+++ trainVelModel.sh
@@ -1,2 +1,2 @@
 
-no_proxy=localhost python trainVal4dVel.py --dataroot /disk/student/adhara/Fall2021/FCNVMB-Deep-learning-based-seismic-velocity-model-building/ --name velModel3 --model New1U --direction AtoB --input_nc 15 --output_nc 1 --display_port 9997 --n_epochs 1 --n_epochs_decay 1 --batch_size 5  --gpu_ids 0,2 --no_html --display_freq 1 --print_freq 200 --lr 0.0001 --verbose --save_epoch_freq 2
+no_proxy=localhost python trainVal4dVel.py --dataroot /disk/student/adhara/Fall2021/FCNVMB-Deep-learning-based-seismic-velocity-model-building/ --name velModel3 --model New1U --direction AtoB --input_nc 15 --output_nc 1 --display_port 9997 --n_epochs 1 --n_epochs_decay 1 --batch_size 5  --gpu_ids 0,1,2 --no_html --display_freq 1 --print_freq 200 --lr 0.0001 --verbose --save_epoch_freq 2
