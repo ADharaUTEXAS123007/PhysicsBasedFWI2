992f3466a241b03d09e28a73d5fa84ca3b5cff2c
Arnab Dhara <adhara@IG-A03506.local>
2021-08-25 15:42:33 -0500

    d2

--- models/New1U_model.py
+++ models/New1U_model.py
@@ -253,7 +253,7 @@ class New1UModel(BaseModel):
         print("epoch numbers : "+str(self.epoch1))
 
 
-    @ray.remote(num_gpus=2)
+    @ray.remote
     def prop(self,epoch1, k):
         #---------deepwave------------#
         
