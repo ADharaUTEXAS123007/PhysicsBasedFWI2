47ad172027cab7b86dbc197dd7d4285186bc02f9
Arnab Dhara <adhara@IG-A03506.local>
2021-08-25 06:53:14 -0500

    testing device

--- models/New1U_model.py
+++ models/New1U_model.py
@@ -51,15 +51,16 @@ class New1UModel(BaseModel):
         """
         BaseModel.__init__(self, opt)
         print("number of cuda devices:", torch.cuda.device_count())
-        #for i in range(3):
-        torch.cuda.set_device(0)
-        torch.cuda.set_device(2)
-        torch.cuda.set_device(1)
+        for i in range(2):
+            torch.cuda.set_device(i)
+
         #torch.cuda.set_device(2)
 
-        self.device1 = torch.device('cuda:{}'.format(self.gpu_ids[0])) if self.gpu_ids else torch.device('cpu') 
-        self.device2 = torch.device('cuda:{}'.format(self.gpu_ids[1])) if self.gpu_ids else torch.device('cpu') 
-        self.device3 = torch.device('cuda:{}'.format(self.gpu_ids[2])) if self.gpu_ids else torch.device('cpu') 
+        for i in range(2):
+            locals()[self.device+str(i+1)] = torch.device('cuda:{}'.format(self.gpu_ids[i])) if self.gpu_ids else torch.device('cpu') 
+            #self.device2 = torch.device('cuda:{}'.format(self.gpu_ids[1])) if self.gpu_ids else torch.device('cpu') 
+        #self.device3 = torch.device('cuda:{}'.format(self.gpu_ids[2])) if self.gpu_ids else torch.device('cpu') 
+        #self.device4 = 
         # specify the training losses you want to print out. The training/test scripts will call <BaseModel.get_current_losses>
         self.loss_names = ['D_MSE','M_MSE','V_MSE']
         # specify the images you want to save/display. The training/test scripts will call <BaseModel.get_current_visuals>
