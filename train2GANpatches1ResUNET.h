434ebf830c6cc23439db39d4791f244b1c9e85a7
Arnab Dhara <adhara@IG-A03506.local>
2021-08-30 19:35:16 -0500

    nepochs1

--- train2GANpatches1ResUNET.h
+++
@@ -1,2 +0,0 @@
-
-no_proxy=localhost python trainVal4d.py --dataroot /glb/data/eptr_am_2/Arnab/volumes/training_data/events/HorizonWindow/att_volumes/patches1/ --name pix2pix2SeismicDRMSPatch1ResUNET --model pix2pix2ResUNET --direction AtoB --input_nc 3 --output_nc 1 --display_port 9997 --n_epochs 1400 --n_epochs_decay 1400 --batch_size 32 --gpu_ids 0,1 --no_html --display_freq 1 --verbose --continue_train
