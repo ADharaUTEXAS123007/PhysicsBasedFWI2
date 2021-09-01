1e79e295f8f9bae78e8287138b6003925d0a4801
Arnab Dhara <adhara@IG-A03506.local>
2021-09-01 01:38:24 -0500

    new1u

--- testVelModel.sh
+++ testVelModel.sh
@@ -1,2 +1,2 @@
 
-python test4d.py --dataroot /disk/student/adhara/Fall2021/FCNVMB-Deep-learning-based-seismic-velocity-model-building/ --name velModel3 --model Auto --direction AtoB --input_nc 15 --output_nc 1 --num_test 1000
+python test4d.py --dataroot /disk/student/adhara/Fall2021/FCNVMB-Deep-learning-based-seismic-velocity-model-building/ --name velModel3 --model New1U --direction AtoB --input_nc 15 --output_nc 1 --num_test 1000
