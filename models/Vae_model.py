d09cb05202cd549b2d5b95df27a6cacfd7cd6e31
Arnab Dhara <adhara@wireless-10-147-176-58.public.utexas.edu>
2021-09-01 13:53:38 -0500

    d

--- models/Vae_model.py
+++ models/Vae_model.py
@@ -168,7 +168,7 @@ class VaeModel(BaseModel):
         #print("real B shape")
         diff_size = self.real_B.size()
         self.loss_M_MSE = (self.criterionMSE(self.fake_B, self.real_B)) * \
-            1000/(diff_size[0]*diff_size[1]*diff_size[2]*diff_size[3])
+            10000/(diff_size[0]*diff_size[1]*diff_size[2]*diff_size[3])
         kld_loss = torch.mean(-0.5 * torch.sum(1 + self.log_var - self.mu ** 2 - self.log_var.exp(), dim = 1), dim = 0)
         self.loss_K_MSE = (7/50) * kld_loss
         #print("KL divergence loss :", kld_loss)
@@ -306,7 +306,7 @@ class VaeModel(BaseModel):
         #print("Loss L1 : "+ str(lossL1.cpu().float().numpy()))
         diff_size = self.real_B.size()
         lossMSE = self.criterionMSE(
-            self.fake_BT, self.real_BT)*1000/(diff_size[0]*diff_size[1]*diff_size[2]*diff_size[3])
+            self.fake_BT, self.real_BT)*10000/(diff_size[0]*diff_size[1]*diff_size[2]*diff_size[3])
         self.loss_V_MSE = lossMSE
         print("Loss RMSE : "+str(lossMSE.cpu().float().numpy()))
         #lossSSIM = metrics.structural_similarity(np.squeeze(self.fake_B.cpu().float().numpy()),np.squeeze(self.real_B.cpu().float().numpy()) )
