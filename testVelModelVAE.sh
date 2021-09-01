18e0935a8869ca4f02a4f95edc98a9dd58ebdc34
Arnab Dhara <adhara@wireless-10-147-176-58.public.utexas.edu>
2021-09-01 09:48:55 -0500

    VAE

---
+++ testVelModelVAE.sh
@@ -0,0 +1,2 @@
+
+python test4d.py --dataroot /disk/student/adhara/Fall2021/FCNVMB-Deep-learning-based-seismic-velocity-model-building/ --name VAE --model VAE --direction AtoB --input_nc 15 --output_nc 1 --num_test 1000
