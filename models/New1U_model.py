4d05bb7b5073698f2cb3f69c7b701ec347c4a895
Arnab Dhara <adhara@IG-A03506.local>
2021-09-04 15:50:26 -0500

    d

--- models/New1U_model.py
+++ models/New1U_model.py
@@ -163,7 +163,7 @@ class New1UModel(BaseModel):
 
     def backward_G1(self, epoch1, batch):
         """Calculate GAN and L1 loss for the generator"""
-        lstart = 70
+        lstart = 50
         diff_size = self.real_B.size()
 
         if (epoch1 > lstart):
