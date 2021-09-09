a2c5f86c0fd1b9541434c2bf5de0274a6da4c75d
Arnab Dhara <adhara@wireless-10-147-26-116.public.utexas.edu>
2021-09-09 11:03:09 -0500

    vae

--- models/Vae_model.py
+++ models/Vae_model.py
@@ -168,7 +168,7 @@ class VaeModel(BaseModel):
         #print("real B shape")
         diff_size = self.real_B.size()
         self.loss_M_MSE = (self.criterionMSE(self.fake_B, self.real_B)) * \
-            10000/(diff_size[0]*diff_size[1]*diff_size[2]*diff_size[3])
+            100/(diff_size[0]*diff_size[1]*diff_size[2]*diff_size[3])
         kld_loss = torch.mean(-0.5 * torch.sum(1 + self.log_var - self.mu ** 2 - self.log_var.exp(), dim = 1), dim = 0)
         self.loss_K_MSE = (7/500) * kld_loss
         #print("KL divergence loss :", kld_loss)
