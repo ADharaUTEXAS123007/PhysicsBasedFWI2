cb5b68779fedbc1414c033058846cc5865225a9d
Arnab Dhara <adhara@IG-A03506.local>
2021-09-11 16:24:33 -0500

    d1

--- models/networks.py
+++ models/networks.py
@@ -2409,7 +2409,7 @@ class Vae_Net(nn.Module):
         source_amplitudes_true = (deepwave.wavelets.ricker(freq, nt, dt, 1/freq)
                                   .reshape(-1, 1, 1))
         #print("device ordinal :", self.devicek)
-        source_amplitudes_true = source_amplitudes_true.to(self.devicek)
+        source_amplitudes_true = source_amplitudes_true.to(devicek)
         #lstart = -1
         num_batches = 1
         num_epochs = 1
