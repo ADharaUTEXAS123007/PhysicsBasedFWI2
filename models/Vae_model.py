1f2a1082319fdff4f2d6bb429d56920a2f9aea93
Arnab Dhara <adhara@IG-A03506.local>
2021-09-11 11:38:32 -0500

    d1

--- models/Vae_model.py
+++ models/Vae_model.py
@@ -241,6 +241,8 @@ class VaeModel(BaseModel):
             self.loss_D_MSE = 0.0
             self.loss_M1_MSE = 0.0
         self.loss_M_MSE = self.criterionMSE(self.fake_B, self.real_B)*100/(diff_size[0]*diff_size[1]*diff_size[2]*diff_size[3])
+        kld_loss = torch.mean(-0.5 * torch.sum(1 + self.log_var - self.mu ** 2 - self.log_var.exp(), dim = 1), dim = 0)
+        self.loss_K_MSE = kld_loss
         #print("loss MSE example :", self.loss_M_MSE)
         #print("diff size :", diff_size)
         
@@ -252,7 +254,7 @@ class VaeModel(BaseModel):
             lambda2 = 0.5
             
 
-        self.loss_G = lambda1 * self.loss_M_MSE + lambda2 * self.loss_M1_MSE
+        self.loss_G = lambda1 * self.loss_M_MSE + self.loss_K_MSE + lambda2 * self.loss_M1_MSE
         #self.loss_G = lambda2 * self.loss_M1_MSE
         self.loss_G.backward()
         #if (epoch1 == 52):
