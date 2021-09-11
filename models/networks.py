d7dedcb876b6a0b45bf43f8cf9eed2b73680fca4
Arnab Dhara <adhara@IG-A03506.local>
2021-09-11 16:43:44 -0500

    d1

--- models/networks.py
+++ models/networks.py
@@ -2429,7 +2429,7 @@ class Vae_Net(nn.Module):
         #net1out1 = net1out1.to(self.devicek)
 
         receiver_amplitudes_true = inputs
-        receiver_amplitudes_true = receiver_amplitudes_true.swapaxes(0,1)
+        #receiver_amplitudes_true = receiver_amplitudes_true.swapaxes(0,1)
         #########rcv_amps_true_max, _ = torch.abs(receiver_amplitudes_true).max(dim=0, keepdim=True)
         ##########rcv_amps_true_norm = receiver_amplitudes_true / (rcv_amps_true_max.abs() + 1e-10)
         rcv_amps_true_norm = receiver_amplitudes_true
@@ -2498,6 +2498,7 @@ class Vae_Net(nn.Module):
                     # Normalize amplitudes by dividing by the maximum amplitude of each receiver
                     ##############batch_rcv_amps_pred_norm = batch_rcv_amps_pred / (batch_rcv_amps_pred_max.abs() + 1e-10)
                     batch_rcv_amps_pred_norm = batch_rcv_amps_pred
+                    
                     #print("shape of receiver amplitudes predicted")
                     # print(np.shape(batch_rcv_amps_pred))
                     lossinner = criterion(batch_rcv_amps_pred_norm, batch_rcv_amps_true)
