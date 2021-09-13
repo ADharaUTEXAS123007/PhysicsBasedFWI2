f8abb5730d21df9d34b34680308ba947eabfae16
Arnab Dhara <adhara@IG-A03506.local>
2021-09-13 09:22:21 -0500

    d

--- test4d.py
+++ test4d.py
@@ -41,37 +41,46 @@ if __name__ == '__main__':
     # hard-code some parameters for test
     opt.num_threads = 0   # test code only supports num_threads = 0
     opt.batch_size = 1    # test code only supports batch_size = 1
-    opt.serial_batches = True  # disable data shuffling; comment this line if results on randomly chosen images are needed.
-    opt.no_flip = True    # no flip; comment this line if results on flipped images are needed.
-    opt.display_id = -1   # no visdom display; the test code saves the results to a HTML file.
-    dataset = create_dataset2(opt)  # create a dataset given opt.dataset_mode and other options
-    model = create_model(opt)      # create a model given opt.model and other options
-    model.setup(opt)               # regular setup: load and print networks; create schedulers
+    # disable data shuffling; comment this line if results on randomly chosen images are needed.
+    opt.serial_batches = True
+    # no flip; comment this line if results on flipped images are needed.
+    opt.no_flip = True
+    # no visdom display; the test code saves the results to a HTML file.
+    opt.display_id = -1
+    # create a dataset given opt.dataset_mode and other options
+    dataset = create_dataset2(opt)
+    # create a model given opt.model and other options
+    model = create_model(opt)
+    # regular setup: load and print networks; create schedulers
+    model.setup(opt)
     # create a website
-    #web_dir = os.path.join(opt.results_dir, opt.name, '{}_{}'.format(opt.phase, opt.epoch))  # define the website directory
-    #if opt.load_iter > 0:  # load_iter is 0 by default
+    # web_dir = os.path.join(opt.results_dir, opt.name, '{}_{}'.format(opt.phase, opt.epoch))  # define the website directory
+    # if opt.load_iter > 0:  # load_iter is 0 by default
     #    web_dir = '{:s}_iter{:d}'.format(web_dir, opt.load_iter)
     #print('creating web directory', web_dir)
     #webpage = html.HTML(web_dir, 'Experiment = %s, Phase = %s, Epoch = %s' % (opt.name, opt.phase, opt.epoch))
     # test with eval mode. This only affects layers like batchnorm and dropout.
     # For [pix2pix]: we use batchnorm and dropout in the original pix2pix. You can experiment it with and without eval() mode.
     # For [CycleGAN]: It should not affect CycleGAN as CycleGAN uses instancenorm without dropout.
-    #if opt.eval:
+    # if opt.eval:
     model.eval()
-    image_dir = '/disk/student/adhara/Fall2021/FCNVMB-Deep-learning-based-seismic-velocity-model-building/Output' + str(1)
-    shutil.rmtree(image_dir)
-    os.mkdir(image_dir)
-    for i, data in enumerate(dataset):
-        print("i :"+str(i))
-        if i >= opt.num_test:  # only apply our model to opt.num_test images.
-            break
-        model.set_input(data)  # unpack data from data loader
-        model.test()           # run inference
-        visuals = model.get_current_visuals()  # get image results
-        img_path = model.get_image_paths()     # get image paths
-        model.compute_loss_only()
-        #if i % 5 == 0:  # save images to an HTML file
-        print('processing (%04d)-th image... %s' % (i, img_path))
-        #save_images(webpage, visuals, img_path, aspect_ratio=opt.aspect_ratio, width=opt.display_winsize)
-        save_matrix(visuals, img_path, aspect_ratio=opt.aspect_ratio, width=opt.display_winsize,mnumber=1)
-    #webpage.save()  # save the HTML
+    for k in enumerate(opt.realization):
+        print("realization :", str(k))
+        image_dir = '/disk/student/adhara/Fall2021/FCNVMB-Deep-learning-based-seismic-velocity-model-building/Output' + str(k)
+        shutil.rmtree(image_dir)
+        os.mkdir(image_dir)
+        for i, data in enumerate(dataset):
+            print("i :"+str(i))
+            if i >= opt.num_test:  # only apply our model to opt.num_test images.
+                break
+            model.set_input(data)  # unpack data from data loader
+            model.test()           # run inference
+            visuals = model.get_current_visuals()  # get image results
+            img_path = model.get_image_paths()     # get image paths
+            model.compute_loss_only()
+            # if i % 5 == 0:  # save images to an HTML file
+            print('processing (%04d)-th image... %s' % (i, img_path))
+            #save_images(webpage, visuals, img_path, aspect_ratio=opt.aspect_ratio, width=opt.display_winsize)
+            save_matrix(visuals, img_path, aspect_ratio=opt.aspect_ratio,
+                        width=opt.display_winsize, mnumber=k)
+    # webpage.save()  # save the HTML
