e50999e061629414ccff256457912773ff938123
Arnab Dhara <adhara@IG-A03506.local>
2021-09-14 18:39:52 -0500

    d

--- models/Vae_model.py
+++ models/Vae_model.py
@@ -286,16 +286,16 @@ class VaeModel(BaseModel):
         #print("diff size :", diff_size)
         #print("device of fake B:",str(self.fake_B.get_device()))
         
-        if (epoch1 > lstart):
-             filen = './deepwave/fake13Sep' + \
-                 str(batch)+'ep'+str(epoch1)+'.npy'
-             np.save(filen, self.fake_B.cpu().detach().numpy())
-             filen = './deepwave/real13Sep' + \
-                 str(batch)+'ep'+str(epoch1)+'.npy'
-             np.save(filen, self.real_B.cpu().detach().numpy())
-             filen = './deepwave/fakeData13Sep' + \
-                    str(batch)+'ep'+str(epoch1)+'.npy'
-             np.save(filen, self.fake_BD.cpu().detach().numpy())
+        if (epoch1%10 == 0):
+            filen = './deepwave/fake13Sep' + \
+                str(batch)+'ep'+str(epoch1)+'.npy'
+            np.save(filen, self.fake_B.cpu().detach().numpy())
+        #     filen = './deepwave/real13Sep' + \
+        #         str(batch)+'ep'+str(epoch1)+'.npy'
+        #     np.save(filen, self.real_B.cpu().detach().numpy())
+        #     filen = './deepwave/fakeData13Sep' + \
+        #            str(batch)+'ep'+str(epoch1)+'.npy'
+        #     np.save(filen, self.fake_BD.cpu().detach().numpy())
 
         lambda1 = 1
         lambda2 = 0
