4463aa15cec5df6149ddf7aa2455f1159be2fc66
Arnab Dhara <adhara@IG-A03506.local>
2021-09-14 11:56:11 -0500

    d

--- models/Vae_model.py
+++ models/Vae_model.py
@@ -272,7 +272,7 @@ class VaeModel(BaseModel):
         diff_size = self.real_B.size()
 
         if (epoch1 > lstart):
-            self.loss_M1_MSE = self.criterionMSE(self.fake_B, self.fake_BD)*100000/(diff_size[0]*diff_size[1]*diff_size[2]*diff_size[3])
+            self.loss_M1_MSE = self.criterionMSE(self.fake_B, self.fake_BD)*10000/(diff_size[0]*diff_size[1]*diff_size[2]*diff_size[3])
         else:
             self.loss_M1_MSE = 0.0
             
