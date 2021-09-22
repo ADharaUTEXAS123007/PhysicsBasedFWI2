e17551476accb2abb65023a22d0e22037934bc89
Arnab Dhara <adhara@IG-A03506.local>
2021-09-22 03:53:36 -0500

    vaenormalizingphy

--- models/networks.py
+++ models/networks.py
@@ -2949,7 +2949,7 @@ class VaeNormalizing_Net(nn.Module):
 class VaeNormalizingPhy_Net(nn.Module):
     def __init__(self, outer_nc, inner_nc, input_nc=None,
                  submodule=None, outermost=False, innermost=False, norm_layer=nn.BatchNorm2d, use_dropout=False):
-        super(VaeNormalizing_Net, self).__init__()
+        super(VaeNormalizingPhy_Net, self).__init__()
         self.is_deconv = True
         self.in_channels = outer_nc
         self.is_batchnorm = True
