90f83c8ed138a4768324686ea1b562e1b2984ca6
Arnab Dhara <adhara@IG-A03506.local>
2021-09-24 13:59:07 -0500

    d

--- models/Vaevel_model.py
+++ models/Vaevel_model.py
@@ -173,7 +173,7 @@ class VaevelModel(BaseModel):
         self.loss_M_MSE = (self.criterionMSE(self.fake_B, self.real_B)) * \
             100/(diff_size[0]*diff_size[1]*diff_size[2]*diff_size[3])
         kld_loss = torch.mean(-0.5 * torch.sum(1 + self.log_var - self.mu ** 2 - self.log_var.exp(), dim = 1), dim = 0)
-        self.loss_K_MSE = kld_loss
+        self.loss_K_MSE = kld_loss/10
         #self.loss_K_MSE = 0.0
 
         #print("KL divergence loss :", kld_loss)
