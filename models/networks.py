616c135a60fed18b5f5bfd0812152e12a8d59b06
Arnab Dhara <adhara@IG-A03506.local>
2021-09-24 07:11:37 -0500

    epoch 240

--- models/networks.py
+++ models/networks.py
@@ -3289,7 +3289,7 @@ class Vaevel_Net(nn.Module):
 
         self.decoder_input = nn.Linear(latent_dim, filters[-2]*13*19)
 
-        self.up4 = autoUp(filters[43, filters[3], self.is_deconv)
+        self.up4 = autoUp(filters[3], filters[3], self.is_deconv)
         self.up3 = autoUp(filters[3], filters[2], self.is_deconv)
         self.up2 = autoUp(filters[2], filters[1], self.is_deconv)
         self.up1 = autoUp(filters[1], filters[0], self.is_deconv)
