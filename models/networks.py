aa056bf339ee53c7ac0a7a5322c15af28a4068b2
Arnab Dhara <adhara@wireless-10-147-164-207.public.utexas.edu>
2021-09-27 09:43:39 -0500

    d

--- models/networks.py
+++ models/networks.py
@@ -2400,7 +2400,7 @@ class Vae_Net(nn.Module):
     def prop(self, inputs, vel, lstart, epoch1):
             #---------deepwave------------#
         net1out1 = vel * 100
-        #print("---shape of vel---", str(np.shape(vel)))
+        print("---shape of vel---", str(np.shape(vel)))
         net1out1 = net1out1.detach()
         net1out1 = torch.squeeze(net1out1)
         devicek = net1out1.get_device()
