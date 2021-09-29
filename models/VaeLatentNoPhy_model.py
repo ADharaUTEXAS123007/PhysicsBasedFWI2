f20334bf5bf815163442072ad13ca7429940a986
Arnab Dhara <adhara@IG-A03506.local>
2021-09-29 13:58:04 -0500

    mu log var

--- models/VaeLatentNoPhy_model.py
+++ models/VaeLatentNoPhy_model.py
@@ -281,10 +281,10 @@ class VaeLatentNoPhyModel(BaseModel):
         
         
         self.loss_D_MSE = 0.0
-        #self.loss_M_MSE = self.criterionMSE(self.fake_B, self.real_B)*100/(diff_size[0]*diff_size[1]*diff_size[2]*diff_size[3])
+        self.loss_M_MSE = self.criterionMSE(self.fake_B, self.real_B)*100/(diff_size[0]*diff_size[1]*diff_size[2]*diff_size[3])
         #kld_loss = torch.mean(-0.5 * torch.sum(1 + self.log_var - self.mu ** 2 - self.log_var.exp(), dim = 1), dim = 0)
         #self.loss_K_MSE = kld_loss
-        self.loss_M_MSE = 0.0
+        #self.loss_M_MSE = 0.0
         self.loss_K_MSE = 0.0
         print("loss_M_MSE : ",self.loss_M_MSE)
         print("loss_K_MSE : ",self.loss_K_MSE)
