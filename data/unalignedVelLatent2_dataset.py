5946c0ecd38db98f1dfbb9f6bbb4dd1d8646df68
Arnab Dhara <adhara@IG-A03506.local>
2021-09-29 13:36:49 -0500

    mu log var

--- data/unalignedVelLatent2_dataset.py
+++ data/unalignedVelLatent2_dataset.py
@@ -7,7 +7,7 @@ import numpy as np
 import torch
 
 
-class UnalignedVel2Dataset(BaseDataset):
+class UnalignedVelLatent2Dataset(BaseDataset):
     """
     This dataset class can load unaligned/unpaired datasets.
 
