1405b69044a4ffc4f927df78a94ff29c197d5ab8
Arnab Dhara <adhara@IG-A03506.local>
2021-09-29 16:02:16 -0500

    mu log var

--- models/VaeLatentNoPhy_model.py
+++ models/VaeLatentNoPhy_model.py
@@ -312,8 +312,8 @@ class VaeLatentNoPhyModel(BaseModel):
                    str(batch)+'ep'+str(epoch1)+'.npy'
              np.save(filen, self.fake_BD.cpu().detach().numpy())
 
-        lambda1 = 1
-        lambda2 = 0
+        lambda1 = 0
+        lambda2 = 1
         #if (epoch1>lstart):
         #    lambda1 = 0.5
         #    lambda2 = 0.5
