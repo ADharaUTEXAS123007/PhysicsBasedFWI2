2ab0f1df0f448cffb42ddc74abe0bc8ffeb1c25f
Arnab Dhara <adhara@IG-A03506.local>
2021-10-03 20:27:00 -0500

    d

--- models/networks.py
+++ models/networks.py
@@ -2982,14 +2982,14 @@ class VaeLatent2NoPhy_Net(nn.Module):
         filters = [64, 128, 256, 512, 1024]
         latent_dim = 256
 
-        self.down1 = unetDown(self.in_channels, filters[0], self.is_batchnorm)
-        self.down2 = unetDown(filters[0], filters[1], self.is_batchnorm)
-        self.down3 = unetDown(filters[1], filters[2], self.is_batchnorm)
-        self.down4 = unetDown(filters[2], filters[3], self.is_batchnorm)
-        #self.center = unetConv2(filters[3], filters[4], self.is_batchnorm)
+        # self.down1 = unetDown(self.in_channels, filters[0], self.is_batchnorm)
+        # self.down2 = unetDown(filters[0], filters[1], self.is_batchnorm)
+        # self.down3 = unetDown(filters[1], filters[2], self.is_batchnorm)
+        # self.down4 = unetDown(filters[2], filters[3], self.is_batchnorm)
+        # #self.center = unetConv2(filters[3], filters[4], self.is_batchnorm)
 
-        self.fc_mu = nn.Linear(filters[-2]*25*7, latent_dim)
-        self.fc_var = nn.Linear(filters[-2]*25*7, latent_dim)
+        # self.fc_mu = nn.Linear(filters[-2]*25*7, latent_dim)
+        # self.fc_var = nn.Linear(filters[-2]*25*7, latent_dim)
         
 
         self.decoder_input = nn.Linear(latent_dim, filters[-2]*26*10)
@@ -3001,25 +3001,25 @@ class VaeLatent2NoPhy_Net(nn.Module):
         self.f1 = nn.Conv2d(filters[0], self.n_classes, 1)
         self.final = nn.ReLU(inplace=True)
 
-    def encode(self, inputs):
-        label_dsp_dim = (151,401)
-        down1 = self.down1(inputs)
-        down2 = self.down2(down1)
-        down3 = self.down3(down2)
-        down4 = self.down4(down3)
-        #center = self.center(down4)
+    # def encode(self, inputs):
+    #     label_dsp_dim = (151,401)
+    #     down1 = self.down1(inputs)
+    #     down2 = self.down2(down1)
+    #     down3 = self.down3(down2)
+    #     down4 = self.down4(down3)
+    #     #center = self.center(down4)
         
-        #print("shape of down")
-        #print(np.shape(down4))
+    #     #print("shape of down")
+    #     #print(np.shape(down4))
 
-        result = torch.flatten(down4, start_dim=1)
-        mu = self.fc_mu(result)
-        log_var = self.fc_var(result)
-        #center = self.center(down4)
+    #     result = torch.flatten(down4, start_dim=1)
+    #     mu = self.fc_mu(result)
+    #     log_var = self.fc_var(result)
+    #     #center = self.center(down4)
 
-        #print("shape of down4")
-        #print(np.shape(down4))
-        return [mu, log_var]
+    #     #print("shape of down4")
+    #     #print(np.shape(down4))
+    #     return [mu, log_var]
 
     def decode(self, inputs):
         filters = [64, 128, 256, 512, 1024]
