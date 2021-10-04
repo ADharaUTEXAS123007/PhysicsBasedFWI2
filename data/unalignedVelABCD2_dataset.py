376b8ab53655e91b452cecfe8c7bccbeea5b6ca9
Arnab Dhara <adhara@IG-A03506.local>
2021-10-04 12:51:35 -0500

    d

--- data/unalignedVelABCD2_dataset.py
+++ data/unalignedVelABCD2_dataset.py
@@ -27,17 +27,17 @@ class UnalignedVelABCD2Dataset(BaseDataset):
         BaseDataset.__init__(self, opt)
         self.dir_A = os.path.join(opt.dataroot, opt.phase + 'A')  # create a path '/path/to/data/trainA'
         self.dir_B = os.path.join(opt.dataroot, opt.phase + 'B')  # create a path '/path/to/data/trainB'
-        self.dir_C = os.path.join(opt.dataroot, opt.phase + 'C')  # create a path '/path/to/data/trainB'
-        self.dir_D = os.path.join(opt.dataroot, opt.phase + 'D')  # create a path '/path/to/data/trainB'
+        #self.dir_C = os.path.join(opt.dataroot, opt.phase + 'C')  # create a path '/path/to/data/trainB'
+        #self.dir_D = os.path.join(opt.dataroot, opt.phase + 'D')  # create a path '/path/to/data/trainB'
 
         self.A_paths = sorted(make_dataset(self.dir_A, opt.max_dataset_size))   # load images from '/path/to/data/trainA'
         self.B_paths = sorted(make_dataset(self.dir_B, opt.max_dataset_size))    # load images from '/path/to/data/trainB'
-        self.C_paths = sorted(make_dataset(self.dir_C, opt.max_dataset_size)) 
-        self.D_paths = sorted(make_dataset(self.dir_D, opt.max_dataset_size)) 
+        #self.C_paths = sorted(make_dataset(self.dir_C, opt.max_dataset_size)) 
+        #self.D_paths = sorted(make_dataset(self.dir_D, opt.max_dataset_size)) 
         self.A_size = len(self.A_paths)  # get the size of dataset A
         self.B_size = len(self.B_paths)  # get the size of dataset B
-        self.C_size = len(self.C_paths)  # get the size of dataset C
-        self.D_size = len(self.D_paths)
+        #self.C_size = len(self.C_paths)  # get the size of dataset C
+        #self.D_size = len(self.D_paths)
         btoA = self.opt.direction == 'BtoA'
         input_nc = self.opt.output_nc if btoA else self.opt.input_nc       # get the number of channels of input image
         output_nc = self.opt.input_nc if btoA else self.opt.output_nc      # get the number of channels of output image
