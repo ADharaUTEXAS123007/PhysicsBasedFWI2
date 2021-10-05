746aade906e25949f81459f55c736ca361d4de26
Arnab Dhara <adhara@IG-A03506.local>
2021-10-05 07:58:17 -0500

    nophy

--- models/VaeNoPhy_model.py
+++ models/VaeNoPhy_model.py
@@ -278,8 +278,8 @@ class VaeNoPhyModel(BaseModel):
             
         
         
-        print("shape of real B :", np.shape(self.real_B))
-        print("shape of fake B : ", np.shape(self.fake_B))
+        #print("shape of real B :", np.shape(self.real_B))
+        #print("shape of fake B : ", np.shape(self.fake_B))
         self.loss_D_MSE = 0.0
         self.loss_M_MSE = self.criterionMSE(self.fake_B, self.real_B)*100/(diff_size[0]*diff_size[1]*diff_size[2]*diff_size[3])
         kld_loss = torch.mean(-0.5 * torch.sum(1 + self.log_var - self.mu ** 2 - self.log_var.exp(), dim = 1), dim = 0)
