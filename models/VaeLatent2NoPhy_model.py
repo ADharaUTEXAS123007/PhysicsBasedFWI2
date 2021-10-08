5f58fb4e7ee15e7b6ba0c5a1569334733ba7bc95
Arnab Dhara <adhara@IG-A03506.local>
2021-10-08 06:08:39 -0500

    d

--- models/VaeLatent2NoPhy_model.py
+++ models/VaeLatent2NoPhy_model.py
@@ -359,7 +359,7 @@ class VaeLatent2NoPhyModel(BaseModel):
         #self.loss_V_L1 = lossL1
         #print("Loss L1 : "+ str(lossL1.cpu().float().numpy()))
         diff_size = self.real_B.size()
-        lossMSE = self.criterionMSE(self.fake_BDT, self.real_A)*100/(diff_size[0]*diff_size[1]*diff_size[2]*diff_size[3])
+        lossMSE = self.criterionMSE(self.fake_BT, self.real_A)*100/(diff_size[0]*diff_size[1]*diff_size[2]*diff_size[3])
         self.loss_V_MSE = lossMSE
         print("Loss RMSE : "+str(lossMSE.cpu().float().numpy()))
         #lossSSIM = metrics.structural_similarity(np.squeeze(self.fake_B.cpu().float().numpy()),np.squeeze(self.real_B.cpu().float().numpy()) )
