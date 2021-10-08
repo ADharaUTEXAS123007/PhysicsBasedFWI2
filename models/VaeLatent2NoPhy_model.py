3ce82414da38ff1147139e86ddd7c5679c42687f
Arnab Dhara <adhara@IG-A03506.local>
2021-10-08 11:22:49 -0500

    d

--- models/VaeLatent2NoPhy_model.py
+++ models/VaeLatent2NoPhy_model.py
@@ -307,9 +307,9 @@ class VaeLatent2NoPhyModel(BaseModel):
         #print("diff size :", diff_size)
         #print("device of fake B:",str(self.fake_B.get_device()))
         
-        #filen = './marmousi/ZZ' + str(batch)+'ep'+str(epoch1)+'.npy'
+        filen = './marmousi/MMT' + str(batch)+'ep'+str(epoch1)+'.npy'
         
-        #np.save(filen, self.z.cpu().detach().numpy()) 
+        np.save(filen, self.fake_B.cpu().detach().numpy()) 
         
         # if (epoch1 > lstart):
         #      filen = './deepwave/fake29Sep' + \
