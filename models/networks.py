dbec1496e31f4e0ca8631ea13dc1f428fa31f49f
Arnab Dhara <adhara@IG-A03506.local>
2021-10-11 06:55:17 -0500

    vae

--- models/networks.py
+++ models/networks.py
@@ -3078,7 +3078,7 @@ class VaeLatent2NoPhy_Net(nn.Module):
         ##mu = 0*de1 #####switch of for physics guided
         ##log_var = 0*de1 #####switch of for physics guided
         de2 = 0*de1
-        z = 0*de1 #####switch of for physics guided
+        ####z = 0*de1 #####switch of for physics guided
         if (epoch1 > lstart):            
             de2 = self.prop(inputs2, de1, lstart, epoch1)
             #de2 = torch.unsqueeze(de2,0)
