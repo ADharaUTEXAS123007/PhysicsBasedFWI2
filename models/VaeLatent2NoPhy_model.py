afe7b5abb3e6f2bdb6329669dfa389aee01c7eb6
Arnab Dhara <adhara@wireless-10-147-193-199.public.utexas.edu>
2021-10-11 11:01:13 -0500

    trainvellatent

--- models/VaeLatent2NoPhy_model.py
+++ models/VaeLatent2NoPhy_model.py
@@ -312,11 +312,11 @@ class VaeLatent2NoPhyModel(BaseModel):
         #print("diff size :", diff_size)
         #print("device of fake B:",str(self.fake_B.get_device()))
         
-        ####filen = './marmousi/ZZ' + str(batch)+'ep'+str(epoch1)+'.npy'
-        ####np.save(filen, self.z.cpu().detach().numpy()) 
+        filen = './marmousi/ZZ' + str(batch)+'ep'+str(epoch1)+'.npy'
+        np.save(filen, self.z.cpu().detach().numpy()) 
         
-        #####filen = './marmousi/MTM' + str(batch)+'ep'+str(epoch1)+'.npy'
-        ######np.save(filen, self.fake_B.cpu().detach().numpy()) 
+        filen = './marmousi/MTM' + str(batch)+'ep'+str(epoch1)+'.npy'
+        np.save(filen, self.fake_B.cpu().detach().numpy()) 
         
         # if (epoch1 > lstart):
         #      filen = './deepwave/fake29Sep' + \
@@ -342,9 +342,9 @@ class VaeLatent2NoPhyModel(BaseModel):
         ######self.loss_G = lambda1 * self.loss_M_MSE + self.loss_K_MSE + lambda2 * self.loss_M1_MSE
         #####self.loss_G = lambda2 * self.loss_M1_MSE
         ########self.loss_G.backward()
-        grad = torch.unsqueeze(torch.unsqueeze(self.fake_BD,0),1) #switch on for physics based fwi
-        grad = grad.to(self.fake_B.get_device()) #switch on for physics based fwi
-        self.fake_B.backward(grad) #switch on for physics based fwi
+        #######grad = torch.unsqueeze(torch.unsqueeze(self.fake_BD,0),1) #switch on for physics based fwi
+        #########grad = grad.to(self.fake_B.get_device()) #switch on for physics based fwi
+        ##########self.fake_B.backward(grad) #switch on for physics based fwi
         
         #############filen = './marmousi/Grad' + str(batch)+'ep'+str(epoch1)+'.npy' #switch on for physics based fwi
         
