3d6f2f22db6037311ea8b96ad5a7d301877b826d
Arnab Dhara <adhara@IG-A03506.local>
2021-10-19 14:47:41 -0500

    d

--- models/Auto2_model.py
+++ models/Auto2_model.py
@@ -244,14 +244,14 @@ class Auto2Model(BaseModel):
             lambda1 = 0.5
             lambda2 = 0.5
             
-
+        self.fake_B.require_grad = True
         self.loss_G = lambda1 * self.loss_M_MSE + lambda2 * self.loss_M1_MSE
         #####self.loss_G = lambda2 * self.loss_M1_MSE
         self.loss_G.backward(retain_graph=True)
         
-        self.fake_B.register_hook(print)
-        #filen = './marmousi/GradNewAD' + str(batch)+'ep'+str(epoch1)+'.npy' #switch on for physics based fwi       
-        #np.save(filen, self.fake_B.grad)  #switch on physics based fwi
+        #self.fake_B.register_hook(print)
+        filen = './marmousi/GradNewAD' + str(batch)+'ep'+str(epoch1)+'.npy' #switch on for physics based fwi       
+        np.save(filen, self.fake_B.grad)  #switch on physics based fwi
         #print("shape of fake B grad :", self.fake_B.grad)
         #grad = torch.unsqueeze(torch.unsqueeze(self.grad,0),1) #switch on for physics based fwi
         #grad = grad.to(self.fake_B.get_device()) #switch on for physics based fwi
