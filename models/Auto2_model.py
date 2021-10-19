53ea830951a825ab1c131bc2a41f05a0b9689cef
Arnab Dhara <adhara@IG-A03506.local>
2021-10-19 13:34:19 -0500

    d

--- models/Auto2_model.py
+++ models/Auto2_model.py
@@ -245,9 +245,9 @@ class Auto2Model(BaseModel):
             lambda2 = 0.5
             
 
-        self.loss_G = lambda1 * self.loss_M_MSE + lambda2 * self.loss_M1_MSE
+        ####self.loss_G = lambda1 * self.loss_M_MSE + lambda2 * self.loss_M1_MSE
         #####self.loss_G = lambda2 * self.loss_M1_MSE
-        self.loss_G.backward(retain_graph=True)
+        #####self.loss_G.backward(retain_graph=True)
         #grad = torch.unsqueeze(torch.unsqueeze(self.grad,0),1) #switch on for physics based fwi
         #grad = grad.to(self.fake_B.get_device()) #switch on for physics based fwi
         self.fake_B.backward(self.grad) #switch on for physics based fwi
