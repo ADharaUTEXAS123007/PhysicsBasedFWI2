254ee5fd4c229aeae3239e2a3fc695671c527059
Arnab Dhara <adhara@IG-A03506.local>
2021-10-21 15:48:02 -0500

    d

--- models/networks.py
+++ models/networks.py
@@ -2326,7 +2326,7 @@ class Auto_Net(nn.Module):
         #print("device ordinal :", self.devicek)
         source_amplitudes_true = source_amplitudes_true.to(devicek)
         #lstart = -1
-        num_batches = 2
+        num_batches = 4
         num_epochs = 1
         if (epoch1 > lstart):
             num_epochs = 1
