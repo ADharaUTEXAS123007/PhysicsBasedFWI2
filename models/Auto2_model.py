962ce30867ab7d42e4ca0e53d23aa2c0480ec24e
Arnab Dhara <adhara@IG-A03506.local>
2021-10-21 10:34:55 -0500

    epoch

--- models/Auto2_model.py
+++ models/Auto2_model.py
@@ -247,7 +247,7 @@ class Auto2Model(BaseModel):
         #self.fake_B.retain_grad()
 
         
-        ##self.loss_G = lambda1 * self.loss_M_MSE + lambda2 * self.loss_M1_MSE
+        self.loss_G = lambda1 * self.loss_M_MSE + lambda2 * self.loss_M1_MSE
         #####self.loss_G = lambda2 * self.loss_M1_MSE
         self.loss_G.backward()
         
