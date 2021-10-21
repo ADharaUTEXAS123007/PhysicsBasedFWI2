9f750f05fe09c39aef9314ee29ea40379697de27
Arnab Dhara <adhara@IG-A03506.local>
2021-10-21 01:11:08 -0500

    epoch

--- models/Auto2_model.py
+++ models/Auto2_model.py
@@ -266,7 +266,7 @@ class Auto2Model(BaseModel):
         #grad = grad.to(self.fake_B.get_device()) #switch on for physics based fwi
         #print("shape of self grad :", np.shape(self.grad))
         
-        self.grad = self.grad
+        self.grad = self.grad/torch.max(self.grad.abs())
         self.fake_B.backward(self.grad) #switch on for physics based fwi
         
         
