3f3b40a4c0cbd4387dc398a02e340b00e3766db9
Arnab Dhara <adhara@IG-A03506.local>
2021-10-23 13:08:11 -0500

    net

--- models/networks.py
+++ models/networks.py
@@ -2445,7 +2445,7 @@ class Auto_Net(nn.Module):
                     net1out1.grad[0:26,:] = 0
                     ##########optimizer2.step()
                     #epoch_loss += loss.item()
-                    #optimizer2.step()
+                    optimizer2.step()
         #if (epoch1 == 52): 
         #print("shape of inputs :", np.shape(inputs))
         #np.save('./marmousi/rcv_amplitudes.npy',batch_rcv_amps_pred.cpu().detach().numpy())
