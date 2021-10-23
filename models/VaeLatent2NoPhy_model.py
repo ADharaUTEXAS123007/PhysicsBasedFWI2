21d994f7f372cffc3f65e8f63d7ec4094d853bad
Arnab Dhara <adhara@IG-A03506.local>
2021-10-23 08:21:07 -0500

    5934

--- models/VaeLatent2NoPhy_model.py
+++ models/VaeLatent2NoPhy_model.py
@@ -345,12 +345,12 @@ class VaeLatent2NoPhyModel(BaseModel):
             lambda2 = 0.5
             
 
-        ########self.loss_G = lambda1 * self.loss_M_MSE + self.loss_K_MSE + lambda2 * self.loss_M1_MSE
+        self.loss_G = lambda1 * self.loss_M_MSE + self.loss_K_MSE + lambda2 * self.loss_M1_MSE
         #self.loss_G = lambda2 * self.loss_M1_MSE
-        ######self.loss_G.backward()
-        grad = torch.unsqueeze(torch.unsqueeze(self.fake_BD,0),1) #switch on for physics based fwi
-        grad = grad.to(self.fake_B.get_device()) #switch on for physics based fwi
-        self.fake_B.backward(grad/100) #switch on for physics based fwi
+        self.loss_G.backward()
+        ####grad = torch.unsqueeze(torch.unsqueeze(self.fake_BD,0),1) #switch on for physics based fwi
+        ###grad = grad.to(self.fake_B.get_device()) #switch on for physics based fwi
+        ###self.fake_B.backward(grad/100) #switch on for physics based fwi
         
         #############filen = './marmousi/Grad' + str(batch)+'ep'+str(epoch1)+'.npy' #switch on for physics based fwi
         #if (epoch1%50 == 0):
