244e46b7da207ec17b4574d29a368023f63c9fc0
Arnab Dhara <adhara@IG-A03506.local>
2021-10-23 01:59:19 -0500

    A_img

--- data/unalignedVelABCD2_dataset.py
+++ data/unalignedVelABCD2_dataset.py
@@ -77,11 +77,11 @@ class UnalignedVelABCD2Dataset(BaseDataset):
         #C_img = C_img
         r = random.randint(0,1)
         
-        print("shape of A 1:", np.shape(A_img))
-        #if (r==0):
-        A_img = np.fliplr(A_img)
-        B_img = np.fliplr(B_img)
-        print("shape of A 2:", np.shape(A_img))
+        #print("shape of A 1:", np.shape(A_img))
+        if (r==0):
+            A_img = np.fliplr(A_img).copy()
+            B_img = np.fliplr(B_img).copy()
+        #print("shape of A 2:", np.shape(A_img))
 
 
         A_img = np.expand_dims(A_img,0)
