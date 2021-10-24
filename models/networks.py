f52c12bc2fd349a40b0e27015d7d4ce947985f82
Arnab Dhara <adhara@IG-A03506.local>
2021-10-24 11:09:11 -0500

    d

--- models/networks.py
+++ models/networks.py
@@ -2216,7 +2216,7 @@ class autoUp(nn.Module):
         #padding=[offset2//2,(offset2+1)//2,offset1//2,(offset1+1)//2]
         # Skip and concatenate 
         #outputs1 = F.pad(inputs1, padding)
-        return self.conv2(outputs2)
+        return outputs2
 
 class Auto_Net(nn.Module):
     def __init__(self,outer_nc, inner_nc, input_nc=None,
