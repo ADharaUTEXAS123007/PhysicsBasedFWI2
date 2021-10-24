f4848f16339e457a46dd82a55898eed87354ee34
Arnab Dhara <adhara@IG-A03506.local>
2021-10-24 12:02:11 -0500

    d

--- models/networks.py
+++ models/networks.py
@@ -2276,7 +2276,7 @@ class Auto_Net(nn.Module):
         up1    = up1[:,:,1:1+label_dsp_dim[0],1:1+label_dsp_dim[1]].contiguous()
         f1     = self.f1(up1)
         f1     = self.f2(f1)
-        f1     = self.f3(f1)
+        #f1     = self.f3(f1)
         f1     = self.final(f1)
         f1     = torch.add(f1,lowf)
         #f1     = 1500 + f1*(3550-1500)
