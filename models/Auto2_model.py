8a498c88639798d32f8e92b542cda9e13783e302
Arnab Dhara <adhara@IG-A03506.local>
2021-10-26 12:28:36 -0500

    10000

--- models/Auto2_model.py
+++ models/Auto2_model.py
@@ -286,12 +286,13 @@ class Auto2Model(BaseModel):
         
         
         #print("shape of fake_B :", np.shape(self.fake_B))
-        #print("shape of grad :", np.shape(self.grad))       
-        filen = './marmousi/GradAD' + str(batch)+'ep'+str(epoch1)+'.npy' #switch on for physics based fwi       
-        np.save(filen, self.grad.cpu().detach().numpy())  #switch on physics based fwi
+        #print("shape of grad :", np.shape(self.grad))   
+        if (epoch1 % 100 == 0): 
+            filen = './marmousi/GradAD' + str(batch)+'ep'+str(epoch1)+'.npy' #switch on for physics based fwi       
+            np.save(filen, self.grad.cpu().detach().numpy())  #switch on physics based fwi
         
-        filen = './marmousi/FakeAD' + str(batch)+'ep'+str(epoch1)+'.npy' #switch on for physics based fwi       
-        np.save(filen, self.fake_B.cpu().detach().numpy())  #switch on physics based fwi
+            filen = './marmousi/FakeAD' + str(batch)+'ep'+str(epoch1)+'.npy' #switch on for physics based fwi       
+            np.save(filen, self.fake_B.cpu().detach().numpy())  #switch on physics based fwi
         
         #filen = './marmousi/RealAD' + str(batch)+'ep'+str(epoch1)+'.npy' #switch on for physics based fwi       
         #np.save(filen, self.real_B.cpu().detach().numpy())  #switch on physics based fwi
