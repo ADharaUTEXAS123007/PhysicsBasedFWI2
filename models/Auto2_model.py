308f8b863eb7e087e5c22061cbce2329b74307b2
Arnab Dhara <adhara@IG-A03506.local>
2021-10-28 22:05:25 -0500

    dp

--- models/Auto2_model.py
+++ models/Auto2_model.py
@@ -142,7 +142,7 @@ class Auto2Model(BaseModel):
     def forward(self,epoch1,lstart):
         """Run forward pass; called by both functions <optimize_parameters> and <test>."""
         #netin1 = self.real_A[:, :, 1:800:2, :]
-        [self.fake_B,self.grad] = self.netG(self.real_B,self.real_A,lstart,epoch1,self.real_B,self.real_B)  # G(A)
+        [self.fake_B,self.grad] = self.netG(self.real_B,self.real_A,lstart,epoch1,self.real_B,self.real_C)  # G(A)
         #self.fake_B = torch.clamp(self.fake_B,min=15.00,max=35.50)
         #filen = './marmousi/Gr1ad' + str(131)+'ep'+str(epoch1)+'.npy' #switch on for physics based fwi       
         #np.save(filen, self.real_A.cpu().detach().numpy())  #switch on physics based fwi
@@ -154,7 +154,7 @@ class Auto2Model(BaseModel):
         False_lstart = 1
         False_epoch = -1
         #netin1 = self.real_A[:, :, 1:800:2, :]
-        [self.fake_BT,self.gradT] = self.netG(self.real_B,self.real_A,False_lstart,False_epoch,self.real_B,self.real_B)  # G(A)
+        [self.fake_BT,self.gradT] = self.netG(self.real_B,self.real_A,False_lstart,False_epoch,self.real_B,self.real_C)  # G(A)
         #self.fake_BT = torch.clamp(self.fake_BT,min=15.00,max=35.50)
         self.real_BT = self.real_B
 
