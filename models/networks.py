1a0be1f4ce792c2bd2e8cb5b05960632a7db7f8d
Arnab Dhara <adhara@IG-A03506.local>
2021-10-30 08:27:47 -0500

    fault test

--- models/networks.py
+++ models/networks.py
@@ -2241,7 +2241,7 @@ class Auto_Net(nn.Module):
         self.down1   = unetDown(self.in_channels, filters[0], self.is_batchnorm)
         self.down2   = unetDown(filters[0], filters[1], self.is_batchnorm)
         self.down3   = unetDown(filters[1], filters[2], self.is_batchnorm)
-        self.down4   = unetDown(filters[2], filters[3], self.is_batchnorm)
+        #self.down4   = unetDown(filters[2], filters[3], self.is_batchnorm)
         # self.center  = unetConv2(filters[3], filters[4], self.is_batchnorm)
         #self.decoder_input1 = nn.Linear(filters[1]*250*51, latent_dim) #for marmousi 151x200
         #self.decoder_input = nn.Linear(latent_dim, filters[3]*250*51) #for marmousi 151x200
@@ -2266,9 +2266,9 @@ class Auto_Net(nn.Module):
         down1  = self.down1(inputs2[:,:,1:800:2,:])
         down2  = self.down2(down1)
         down3  = self.down3(down2)
-        down4  = self.down4(down3)
+        #down4  = self.down4(down3)
         
-        print("shape of down3 :", np.shape(down4))
+        print("shape of down3 :", np.shape(down3))
         
         #print("shape of down2 :", np.shape(down2))
         result = torch.flatten(down2, start_dim=1)
@@ -2285,9 +2285,9 @@ class Auto_Net(nn.Module):
         
         #p = torch.randn([1,1,1,8])
         
-        z = self.decoder_input(p)
+        #z = self.decoder_input(p)
         #z = z.view(-1, filters[3], 250, 51) #for marmousi model
-        z = z.view(-1, filters[3], 100, 26)
+        z = p.view(-1, filters[3], 100, 26)
     
         up1    = self.up3(z)
         up1    = self.up2(up1)
