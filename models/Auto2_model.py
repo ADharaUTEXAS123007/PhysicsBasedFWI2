a2609bfad4e8e9f2a791af33d88be62263476d2e
Arnab Dhara <adhara@IG-A03506.local>
2021-11-02 11:40:29 -0500

    f1

--- models/Auto2_model.py
+++ models/Auto2_model.py
@@ -156,7 +156,7 @@ class Auto2Model(BaseModel):
         False_lstart = 1
         False_epoch = -1
         #netin1 = self.real_A[:, :, 1:800:2, :]
-        [self.fake_BT,self.gradT] = self.netG(self.real_C,self.real_A,False_lstart,False_epoch,self.real_B,self.real_C)  # G(A)
+        [self.fake_BT,self.gradT] = self.netG(self.real_B,self.real_A,False_lstart,False_epoch,self.real_B,self.real_C)  # G(A)
         #self.fake_BT = torch.clamp(self.fake_BT,min=15.00,max=35.50)
         self.real_BT = self.real_B
         #self.real_C = self.real_BT
