6d18e74bf8b9bd47f2ffdc47b238cc9fda382a46
Arnab Dhara <adhara@IG-A03506.local>
2021-11-02 07:27:32 -0500

    f1

--- test4d.py
+++ test4d.py
@@ -83,6 +83,6 @@ if __name__ == '__main__':
             # if i % 5 == 0:  # save images to an HTML file
             print('processing (%04d)-th image... %s' % (i, img_path))
             #save_images(webpage, visuals, img_path, aspect_ratio=opt.aspect_ratio, width=opt.display_winsize)
-            save_matrix(visuals, img_path, aspect_ratio=opt.aspect_ratio,
-                        width=opt.display_winsize, mnumber=k)
+            save_matrix(visuals, img_path, k, aspect_ratio=opt.aspect_ratio,
+                        width=opt.display_winsize)
     # webpage.save()  # save the HTML
