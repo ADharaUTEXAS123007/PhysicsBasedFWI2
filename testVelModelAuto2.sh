5a83b50644a0d41a34295ef73b02478788881013
Arnab Dhara <adhara@IG-A03506.local>
2021-11-01 20:24:19 -0500

    d

--- testVelModelAuto2.sh
+++ testVelModelAuto2.sh
@@ -1,2 +1,2 @@
 
-python test4d.py --dataroot /disk/student/adhara/Fall2021/FCNVMB-Deep-learning-based-seismic-velocity-model-building/Fault/ --name AutoModelFault --model Auto2 --direction AtoB --input_nc 12 --output_nc 1 --num_test 1
+python test4d.py --dataroot /disk/student/adhara/Fall2021/FCNVMB-Deep-learning-based-seismic-velocity-model-building/Fault/ --name AutoModelFault --model Auto2 --direction AtoB --input_nc 8 --output_nc 1 --num_test 1
