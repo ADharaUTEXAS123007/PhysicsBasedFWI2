896f410122f87ddb5ebffed71f4fa1089164b30b
Arnab Dhara <adhara@IG-A03506.local>
2021-11-03 15:22:53 -0500

    auto2

--- models/networks.py
+++ models/networks.py
@@ -2379,9 +2379,9 @@ class Auto_Net(nn.Module):
         source_amplitudes_true = source_amplitudes_true.to(devicek)
         #lstart = -1
         num_batches = 1
-        num_epochs = 300
+        num_epochs = 1
         if (epoch1 > lstart):
-            num_epochs = 300
+            num_epochs = 1
         #if (epoch1 > 50):
         #    num_epochs = 30
         #if (epoch1 > 80):
