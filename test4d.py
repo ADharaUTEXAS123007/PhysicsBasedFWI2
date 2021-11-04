d760844a17b092fcbd2999350839267a7994c201
Arnab Dhara <adhara@IG-A03506.local>
2021-11-04 10:27:14 -0500

    vae

--- test4d.py
+++ test4d.py
@@ -39,7 +39,7 @@ import shutil
 if __name__ == '__main__':
     opt = TestOptions().parse()  # get test options
     # hard-code some parameters for test
-    opt.realization = 1
+    #opt.realization = 1
     opt.num_threads = 0   # test code only supports num_threads = 0
     opt.batch_size = 1    # test code only supports batch_size = 1
     # disable data shuffling; comment this line if results on randomly chosen images are needed.
