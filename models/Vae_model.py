40aca52a16ac1c63f082ae7bb8b8791a6dd716ac
Arnab Dhara <adhara@IG-A03506.local>
2021-11-03 21:04:24 -0500

    vae

--- models/Vae_model.py
+++ models/Vae_model.py
@@ -328,7 +328,7 @@ class VaeModel(BaseModel):
         #            str(batch)+'ep'+str(epoch1)+'.npy'
         #     np.save(filen, self.fake_BD.cpu().detach().numpy())
 
-        lambda1 = 10000
+        lambda1 = 100000
         lambda2 = 0
         if (epoch1>lstart):
             lambda1 = 0.5
