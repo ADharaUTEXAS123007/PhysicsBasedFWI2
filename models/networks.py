7b212910dba400a208cfb2cb86fb937d30873b06
Arnab Dhara <adhara@IG-A03506.local>
2021-11-05 11:31:44 -0500

    d

--- models/networks.py
+++ models/networks.py
@@ -2802,7 +2802,7 @@ class Vae_Net(nn.Module):
                     ##########    sumlossinner += lossinner.item()
                     #########if (epoch1 > lstart):
                     lossinner.backward()
-                    #net1out1.grad[0:26,:] = 0
+                    net1out1.grad[0:4,:] = 0
                     ##########optimizer2.step()
                     #epoch_loss += loss.item()
                     #optimizer2.step()
