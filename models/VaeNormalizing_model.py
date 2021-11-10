28a2571a14082b8baca07d7c9cddb0d318a445ce
Arnab Dhara <adhara@IG-A03506.local>
2021-11-09 19:52:09 -0600

    normalizing

--- models/VaeNormalizing_model.py
+++ models/VaeNormalizing_model.py
@@ -282,7 +282,7 @@ class VaeNormalizingModel(BaseModel):
         
         
         self.loss_D_MSE = 0.0
-        self.loss_M_MSE = self.criterionMSE(self.fake_B, self.real_B)*100/(diff_size[0]*diff_size[1]*diff_size[2]*diff_size[3])
+        self.loss_M_MSE = self.criterionMSE(self.fake_B, self.real_B)/(diff_size[0]*diff_size[1]*diff_size[2]*diff_size[3])
         #kld_loss = torch.mean(-0.5 * torch.sum(1 + self.log_var - self.mu ** 2 - self.log_var.exp(), dim = 1), dim = 0)
         self.loss_K_MSE = torch.mean(self.kld_loss)
         #print("kld loss :",self.loss_K_MSE)
