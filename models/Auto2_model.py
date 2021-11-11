f746b74114d4309de516c828b0e5a861905a73f4
Arnab Dhara <adhara@IG-A03506.local>
2021-11-10 18:47:56 -0600

    epoch

--- models/Auto2_model.py
+++ models/Auto2_model.py
@@ -279,8 +279,8 @@ class Auto2Model(BaseModel):
             #self.fake_B.grad = None
             #self.fake_B.grad = None
             #if (epoch1>lstart and epoch1<=lstart1):
-            self.grad = self.grad   #####(10**5) works for marmousi model
-            #self.grad = torch.clip(self.grad, min=-0.1, max=0.1)
+            self.grad = self.grad*(10**7)   #####(10**5) works for marmousi model
+            self.grad = torch.clip(self.grad, min=-0.1, max=0.1)
                 
             #if (epoch1>lstart1 and epoch1<=lstart2):
             #    self.grad = self.grad*(10**6)  #####(10**5) works for marmousi model
