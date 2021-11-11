86fdcb1adf556a9cbb42dc8024df925c208901bd
Arnab Dhara <adhara@IG-A03506.local>
2021-11-11 15:51:36 -0600

    latent

--- models/networks.py
+++ models/networks.py
@@ -2286,7 +2286,7 @@ class Auto_Net(nn.Module):
         #up2    = self.up2(up3)
         #print("shape of down 4:", np.shape(down2))
         #print("shape of result:", np.shape(result))
-        if (epoch1 <= 10):
+        if (epoch1 <= lstart):
             latent1 = p
         else:
             p = latentI
