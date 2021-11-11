d18b6999d084efed42bf71755dd2eb7c27437e4a
Arnab Dhara <adhara@IG-A03506.local>
2021-11-11 15:40:29 -0600

    latent

--- models/Auto2_model.py
+++ models/Auto2_model.py
@@ -143,7 +143,7 @@ class Auto2Model(BaseModel):
         """Run forward pass; called by both functions <optimize_parameters> and <test>."""
         #netin1 = self.real_A[:, :, 1:800:2, :]
         if (epoch1 == 1):
-            self.latent = torch.ones([1,1,1,1])
+            self.latent = torch.ones(1,1,1,1)
         [self.fake_B,self.grad,self.latent] = self.netG(self.real_B,self.real_A,lstart,epoch1,self.latent,self.real_C)  # G(A)
         #self.real_C = self.fake_B
         #self.real_B = self.real_C
@@ -159,7 +159,7 @@ class Auto2Model(BaseModel):
         False_epoch = -1
         #netin1 = self.real_A[:, :, 1:800:2, :]
         #if (epoch1 == 1):
-        self.latentT = torch.ones([1,1,1,1])
+        self.latentT = torch.ones(1,1,1,1)
         [self.fake_BT,self.gradT,self.latentT] = self.netG(self.real_B,self.real_A,False_lstart,False_epoch,self.latentT,self.real_C)  # G(A)
         #self.fake_BT = torch.clamp(self.fake_BT,min=15.00,max=35.50)
         self.real_BT = self.real_B
