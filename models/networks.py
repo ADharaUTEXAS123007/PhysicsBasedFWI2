b279f05df21298b09f368ade401d00c996ffaa19
Arnab Dhara <adhara@IG-A03506.local>
2021-11-10 19:03:55 -0600

    epoch

--- models/networks.py
+++ models/networks.py
@@ -2463,7 +2463,7 @@ class Auto_Net(nn.Module):
                     #if (epoch1 > lstart):
                     optimizer2.zero_grad()
                     model2 = net1out1.clone()
-                    #model2 = torch.clamp(net1out1,min=3000,max=6000)
+                    model2 = torch.clamp(net1out1,min=3000,max=6000)
                     #np.save('before108.npy',net1out1.cpu().detach().numpy())
                     #net1out1 = torch.clamp(net1out1,min=2000,max=4500)
                     prop = deepwave.scalar.Propagator({'vp': model2}, dx)
