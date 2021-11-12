abf5b8670398b3d35afe05dc9c4d862072f7f625
Arnab Dhara <adhara@IG-A03506.local>
2021-11-11 19:18:42 -0600

    latent

--- trainValLatent4dVel2.py
+++ trainValLatent4dVel2.py
@@ -45,7 +45,7 @@ if __name__ == '__main__':
     visualizer = Visualizer(opt)   # create a visualizer that display/save images and plots
     total_iters = 0                # the total number of training iterations
     losses1 = OrderedDict()
-    lstart = 200
+    lstart = 60
     for epoch in range(opt.epoch_count, opt.n_epochs + opt.n_epochs_decay + 1):    # outer loop for different epochs; we save the model by <epoch_count>, <epoch_count>+<save_latest_freq>
          epoch_start_time = time.time()  # timer for entire epoch
          iter_data_time = time.time()    # timer for data loading per iteration
