dc4ead01d4c2333e5b956aecef32387fe53fc80b
Arnab Dhara <adhara@IG-A03506.local>
2021-11-14 01:37:10 -0600

    phy

--- models/networks.py
+++ models/networks.py
@@ -2424,7 +2424,7 @@ class Auto_Net(nn.Module):
         #print(min1.get_device())
         #min1 = min1.to(self.device1)
         mat2 = torch.ones(net1out1.size()[0],net1out1.size()[1]).to(devicek)
-        mat2 = mat2 * 3885
+        mat2 = mat2 * 3000
         #mat2 = torch.clamp(mat2,min=1500,max=3550)
         #min1 = torch.min(net1out1)
         #max1 = torch.max(net1out1)
