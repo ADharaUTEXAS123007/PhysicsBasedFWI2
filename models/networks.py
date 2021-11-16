cc69b8b46efd6012a527b4ccc5e19db58ceddbb6
Arnab Dhara <adhara@IG-A03506.local>
2021-11-15 21:08:56 -0600

    mintrue

--- models/networks.py
+++ models/networks.py
@@ -2656,7 +2656,7 @@ class AutoMarmousi_Net(nn.Module):
         #z = 0.5*torch.ones([1,1,1,64])
         z = self.decoder_input(p)
         #z = z.view(-1, filters[3], 250, 51) #for marmousi model
-        z = z.view(-1, filters[3], 50, 13)
+        z = z.view(-1, filters[3], 13, 50)
     
         up1    = self.up3(z)
         #print(" shape of up1 :", np.shape(up1))
