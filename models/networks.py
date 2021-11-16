95ad9cf9f6faf2d6947e36e7e8d3ab8cba1db58d
Arnab Dhara <adhara@IG-A03506.local>
2021-11-15 20:06:34 -0600

    mintrue

--- models/networks.py
+++ models/networks.py
@@ -2717,16 +2717,16 @@ class AutoMarmousi_Net(nn.Module):
     def prop(self, inputs, vel, lstart, epoch1, mintrue, maxtrue):
         
         #torch.cuda.set_device(7)  #RB Necessary if device <> 0
-        #GPU_string='cuda:'+str(7)
-        #devicek = torch.device(GPU_string)
+        GPU_string='cuda:'+str(7)
+        devicek = torch.device(GPU_string)
         net1out1 = mintrue + vel*(maxtrue-mintrue)
         net1out1 = vel*1000
         #net1out1 = (3550-1500)*vel+1500
         #print("---shape of vel---", str(np.shape(vel)))
         net1out1 = net1out1.detach()
         net1out1 = torch.squeeze(net1out1)
-        #net1out1 = net1out1.to(devicek)
-        devicek = net1out1.get_device()
+        net1out1 = net1out1.to(devicek)
+        #devicek = net1out1.get_device()
         #net1out1[0:26,:] = 1500.0
 
         
