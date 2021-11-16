da9cd8b84245999f136a4e9d04f82974afcf5561
Arnab Dhara <adhara@IG-A03506.local>
2021-11-16 13:32:33 -0600

    mintrue

--- models/networks.py
+++ models/networks.py
@@ -2367,7 +2367,7 @@ class Auto_Net(nn.Module):
         #f1     = torch.clamp(f1,min=20,max=45)
         
         grad = 0*f1
-        lossT = [0.0]
+        lossT = 0*f1
         if (epoch1 > lstart):
             [grad, lossT] = self.prop(inputs2, f1, lstart, epoch1, mintrue, maxtrue)
             grad = torch.unsqueeze(grad,0)
