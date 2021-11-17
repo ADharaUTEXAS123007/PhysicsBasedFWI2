42cb3c89bd16eb3e52daf1e94d1e3554e1166603
Arnab Dhara <adhara@IG-A03506.local>
2021-11-17 12:45:04 -0600

    epcoh

--- trainVelAuto2ModelPhy.sh
+++ trainVelAuto2ModelPhy.sh
@@ -1,3 +1,3 @@
 
 #rm -rf /disk/student/adhara/Fall2021/PhysicsBasedFWI/checkpoints/Auto2SaltPhy64/*.txt
-no_proxy=localhost python trainValLatent4dVel2.py --dataroot /disk/student/adhara/Fall2021/FCNVMB-Deep-learning-based-seismic-velocity-model-building/Marmousi/ --name AutoOpenFWI --model Auto2 --direction AtoB --input_nc 18 --output_nc 1 --display_port 9996 --n_epochs 500 --n_epochs_decay 200  --batch_size 8 --gpu_ids 0 --no_html --display_freq 1 --print_freq 1 --lr 0.01 --verbose --save_epoch_freq 100
+no_proxy=localhost python trainValLatent4dVel2.py --dataroot /disk/student/adhara/Fall2021/FCNVMB-Deep-learning-based-seismic-velocity-model-building/Marmousi/ --name AutoOpenFWI --model Auto2 --direction AtoB --input_nc 18 --output_nc 1 --display_port 9996 --n_epochs 500 --n_epochs_decay 200  --batch_size 8 --gpu_ids 7 --no_html --display_freq 1 --print_freq 1 --lr 0.01 --verbose --save_epoch_freq 100
