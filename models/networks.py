857173ea51fdf54c99c393d0433d2b61fb7535df
Arnab Dhara <adhara@IG-A03506.local>
2021-11-19 13:44:42 -0600

    physics

--- models/networks.py
+++ models/networks.py
@@ -2947,7 +2947,7 @@ class AutoN_Net(nn.Module):
 
     def forward(self, inputs1, inputs2, lstart, epoch1, p, lowf):
         filters = [16, 32, 64, 128, 512]
-        latent_dim = 128
+        latent_dim = 8
         label_dsp_dim = (70,70)
         mintrue = torch.min(inputs1)
         maxtrue = torch.max(inputs1)
@@ -2956,9 +2956,9 @@ class AutoN_Net(nn.Module):
         down1  = self.down1(inputs2)
         down2  = self.down2(down1)
         down3  = self.down3(down2)
-        #down4  = self.down4(down3)
+        down4  = self.down4(down3)
         
-        #print("shape of down3 :", np.shape(down3))
+        print("shape of down4 :", np.shape(down4))
         
         #print("shape of down2 :", np.shape(down2))
         result = torch.flatten(down3, start_dim=1)
