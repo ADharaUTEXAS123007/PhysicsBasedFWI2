790dccc633c2aeeb2d05f3f5ed6d1d95cdd5bf0b
Arnab Dhara <adhara@IG-A03506.local>
2021-11-19 04:25:51 -0600

    physics

--- models/networks.py
+++ models/networks.py
@@ -3149,9 +3149,9 @@ class AutoN_Net(nn.Module):
         rcv_amps_true_norm = receiver_amplitudes_true / (rcv_amps_true_max.abs() + 1e-10)
         #rcv_amps_true_norm = receiver_amplitudes_true
 
-        criterion1 = torch.nn.MSELoss()
+        criterion1 = torch.nn.L1Loss()
         #vgg = Vgg16().type(torch.cuda.FloatTensor)
-        #criterion2 = torch.nn.MSELoss()
+        criterion2 = torch.nn.MSELoss()
         #print("shape of mat2 :", np.shape(mat2))
         
 
@@ -3205,8 +3205,8 @@ class AutoN_Net(nn.Module):
                     #print("shape of receiver amplitudes predicted")
                     # print(np.shape(batch_rcv_amps_pred))
                     lossinner1 = criterion1(batch_rcv_amps_pred_norm, batch_rcv_amps_true)
-                    #lossinner2 = criterion2(batch_rcv_amps_pred_norm, batch_rcv_amps_true)
-                    lossinner = lossinner1
+                    lossinner2 = torch,sqrt(criterion2(batch_rcv_amps_pred_norm, batch_rcv_amps_true))
+                    lossinner = lossinner1 + lossinner2
                     #y_c_features = vgg(torch.unsqueeze(batch_rcv_amps_true,0))
                     #########model2.grad[0:26,:] = 0
                     #filen = './deepwave/epoch1'+str(epoch)+'.npy'
