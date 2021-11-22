f5b457cc4922affa82d86d8326fb5633a7b2c7db
Arnab Dhara <adhara@wireless-10-147-131-172.public.utexas.edu>
2021-11-22 12:36:23 -0600

    physics

--- models/networks.py
+++ models/networks.py
@@ -2605,7 +2605,7 @@ class AutoMarmousi_Net(nn.Module):
         ##self.decoder_input1 = nn.Linear(filters[1]*250*51, latent_dim) #for marmousi 151x200
         #self.decoder_input1 = nn.Linear(filters[2]*125*26, latent_dim) #for marmousi 151x200
         #self.decoder_input = nn.Linear(latent_dim, filters[2]*500*102) #for marmousi 151x200
-        self.decoder_input1 = nn.Linear(filters[2]*125*25, latent_dim) #for marmousi 101x101
+        self.decoder_input1 = nn.Linear(filters[2]*63*13, latent_dim) #for marmousi 101x101
         #self.decoder_input = nn.Linear(latent_dim, filters[3]*100*26) #for marmousi 101x101
         #self.decoder_input1 = nn.Linear(filters[1]*100*18, latent_dim) #for marmousi 101x101
         self.decoder_input = nn.Linear(latent_dim, filters[3]*25*13) #for marmousi 101x101
