031a25aea4b3dd0b6569478dd1b58c4f01d1ab9f
Arnab Dhara <adhara@IG-A03506.local>
2021-11-23 07:40:00 -0600

    tanh

--- models/networks.py
+++ models/networks.py
@@ -3170,7 +3170,7 @@ class AutoMarmousi21_Net(nn.Module):
 
         criterion1 = torch.nn.L1Loss()
         #vgg = Vgg16().type(torch.cuda.FloatTensor)
-        criterion2 = torch.nn.MSELoss()
+        #criterion2 = torch.nn.MSELoss()
         #print("shape of mat2 :", np.shape(mat2))
         
 
@@ -3223,8 +3223,8 @@ class AutoMarmousi21_Net(nn.Module):
                     #print("shape of receiver amplitudes predicted")
                     # print(np.shape(batch_rcv_amps_pred))
                     lossinner1 = criterion1(batch_rcv_amps_pred_norm, batch_rcv_amps_true)
-                    lossinner2 = criterion2(batch_rcv_amps_pred_norm, batch_rcv_amps_true)
-                    lossinner = torch.sqrt(lossinner2) + lossinner1
+                    #lossinner2 = criterion2(batch_rcv_amps_pred_norm, batch_rcv_amps_true)
+                    lossinner = lossinner1
                     #y_c_features = vgg(torch.unsqueeze(batch_rcv_amps_true,0))
                     #########model2.grad[0:26,:] = 0
                     #filen = './deepwave/epoch1'+str(epoch)+'.npy'
