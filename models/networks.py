9c253d7c3f38095a1fdd535b9d9fe1a4d1da02e2
Arnab Dhara <adhara@IG-A03506.local>
2021-11-25 02:58:40 -0600

    marmo

--- models/networks.py
+++ models/networks.py
@@ -3476,7 +3476,7 @@ class AutoMarmousi21_Net(nn.Module):
         dx = 10
         nt = 4001
         dt = 0.001
-        num_shots = 16
+        num_shots = 18
         num_receivers_per_shot = 200
         num_sources_per_shot = 1
         num_dims = 2
