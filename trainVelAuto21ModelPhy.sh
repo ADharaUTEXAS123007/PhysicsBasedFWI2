70d9fcf3b2ab18edc52118f02bc9820d9c78f7db
Arnab Dhara <adhara@IG-A03506.local>
2021-11-25 02:57:52 -0600

    marmo

--- trainVelAuto21ModelPhy.sh
+++ trainVelAuto21ModelPhy.sh
@@ -1,4 +1,4 @@
 
 mkdir ./marmousi21
 rm -rf /disk/student/adhara/Fall2021/PhysicsBasedFWI/checkpoints/MarmousiOpenFWI21/*.txt
-no_proxy=localhost python trainValLatent4dVel2.py --dataroot /disk/student/adhara/Fall2021/FCNVMB-Deep-learning-based-seismic-velocity-model-building/MarmousiT/ --name MarmousiOpenFWI21 --model Auto21 --direction AtoB --input_nc 16 --output_nc 1 --display_port 9996 --n_epochs 2000 --n_epochs_decay 1000  --batch_size 8 --gpu_ids 7 --no_html --display_freq 1 --print_freq 1 --lr 0.01 --verbose --save_epoch_freq 25
+no_proxy=localhost python trainValLatent4dVel2.py --dataroot /disk/student/adhara/Fall2021/FCNVMB-Deep-learning-based-seismic-velocity-model-building/MarmousiT/ --name MarmousiOpenFWI21 --model Auto21 --direction AtoB --input_nc 18 --output_nc 1 --display_port 9996 --n_epochs 2000 --n_epochs_decay 1000  --batch_size 8 --gpu_ids 7 --no_html --display_freq 1 --print_freq 1 --lr 0.01 --verbose --save_epoch_freq 25
