189ae33d3391a115820ac9f622e1337a637e5f99
Arnab Dhara <adhara@IG-A03506.local>
2021-11-25 03:18:06 -0600

    down3

--- models/networks.py
+++ models/networks.py
@@ -3424,6 +3424,8 @@ class AutoMarmousi21_Net(nn.Module):
         lossT = 0.0
         if (epoch1 > lstart):
             [grad, lossT] = self.prop(inputs2, f1, lstart, epoch1, mintrue, maxtrue, inputs1)
+            grad = grad.to(inputs2.get_device())
+            lossT = lossT.to(inputs2.get_device())
             grad = torch.unsqueeze(grad,0)
             grad = torch.unsqueeze(grad,0)
         #result = torch.flatten(f1, start_dim=1)
