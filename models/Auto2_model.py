e875f0ded495c8d9482519a7edc799b255db4d84
Arnab Dhara <adhara@IG-A03506.local>
2021-11-26 13:02:19 -0600

    marmousi 1

--- models/Auto2_model.py
+++ models/Auto2_model.py
@@ -227,7 +227,7 @@ class Auto2Model(BaseModel):
         
         self.loss_M_MSE = self.criterionMSE(self.real_B, self.fake_B)/(diff_size[0]*diff_size[1]*diff_size[2]*diff_size[3])
         
-        wavelet = wav.ormsby(0.2,1e-3,[30,40,70,90])
+        wavelet = wav.ricker(0.2,2e-3,20)
         wavelet = torch.tensor(wavelet).unsqueeze(dim=0).unsqueeze(dim=0).float().to(self.real_B.get_device())
         tr1 = self.real_A * 0
         
