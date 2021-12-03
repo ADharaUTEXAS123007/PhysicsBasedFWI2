6e29cd93b7281c50edb52780e2ab0d77ce418242
Arnab Dhara <adhara@IG-A03506.local>
2021-12-03 07:03:11 -0600

    23

--- testVelModelVae2ModelPhy.sh
+++ testVelModelVae2ModelPhy.sh
@@ -1,2 +1,2 @@
 
-python test4d.py --dataroot /disk/student/adhara/Fall2021/FCNVMB-Deep-learning-based-seismic-velocity-model-building/MarmousiF/ --name MarmousiOpenFWI3Vae --model Vae3 --direction AtoB --input_nc 18 --output_nc 1 --realization 30 --epoch 2000
+python test4d.py --dataroot /disk/student/adhara/Fall2021/FCNVMB-Deep-learning-based-seismic-velocity-model-building/MarmousiF/ --name MarmousiOpenFWI3Vae --model Vae3 --direction AtoB --input_nc 18 --output_nc 1 --realization 50 --epoch 2000
