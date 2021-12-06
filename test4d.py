45983d82e545ec6983e7e0836df549b83ebc3ad9
Arnab Dhara <adhara@wireless-10-147-169-24.public.utexas.edu>
2021-12-06 11:34:07 -0600

    vae

--- test4d.py
+++ test4d.py
@@ -76,7 +76,7 @@ if __name__ == '__main__':
 #            if i >= opt.num_test:  # only apply our model to opt.num_test images.
 #                break
             model.set_input(data)  # unpack data from data loader
-            model.test()           # run inference
+            #model.test()           # run inference
             visuals = model.get_current_visuals()  # get image results
             img_path = model.get_image_paths()     # get image paths
             model.compute_loss_only()
