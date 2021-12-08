094c6950e47b66c91a7e906879621584207aaae4
Arnab Dhara <adhara@IG-A03506.local>
2021-12-08 02:54:55 -0600

    seam

--- models/networks.py
+++ models/networks.py
@@ -2290,7 +2290,7 @@ class autoUp2(nn.Module):
         #padding=[offset2//2,(offset2+1)//2,offset1//2,(offset1+1)//2]
         # Skip and concatenate 
         #outputs1 = F.pad(inputs1, padding)
-        return self.conv2(outputs3)
+        return outputs3
 
 class Auto_Net(nn.Module):
     def __init__(self,outer_nc, inner_nc, input_nc=None,
