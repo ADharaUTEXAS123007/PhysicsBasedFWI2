dacd2bb0a352d985886ead6e28a4856df33d8844
Arnab Dhara <adhara@IG-A03506.local>
2021-12-08 20:03:52 -0600

    kaiming

--- trainVelAuto23ModelPhy.sh
+++ trainVelAuto23ModelPhy.sh
@@ -1,4 +1,4 @@
 
 mkdir ./marmousi23
 rm -rf /disk/student/adhara/Fall2021/PhysicsBasedFWI/checkpoints/MarmousiOpenFWI23/*.txt
-no_proxy=localhost python trainValLatent4dVel2.py --dataroot /disk/student/adhara/Fall2021/FCNVMB-Deep-learning-based-seismic-velocity-model-building/SEAM/ --name MarmousiOpenFWI23 --model Auto23 --direction AtoB --input_nc 18 --output_nc 1 --display_port 9991 --n_epochs 1500 --n_epochs_decay 2000  --batch_size 8 --gpu_ids 3 --no_html --display_freq 1 --print_freq 1 --lr 0.01 --verbose --save_epoch_freq 25 --init_type kaiming
+no_proxy=localhost python trainValLatent4dVel2.py --dataroot /disk/student/adhara/Fall2021/FCNVMB-Deep-learning-based-seismic-velocity-model-building/SEAM/ --name MarmousiOpenFWI23 --model Auto23 --direction AtoB --input_nc 18 --output_nc 1 --display_port 9991 --n_epochs 1500 --n_epochs_decay 2000  --batch_size 8 --gpu_ids 3 --no_html --display_freq 1 --print_freq 1 --lr 0.01 --verbose --save_epoch_freq 25 --init_type xavier
