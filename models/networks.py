ede6d63a60c1f8199beb602c8088542fbc7f3f5e
Arnab Dhara <adhara@IG-A03506.local>
2021-12-08 19:52:50 -0600

    kaiming

--- models/networks.py
+++ models/networks.py
@@ -4424,7 +4424,7 @@ class AutoMarmousi23_Net(nn.Module):
         self.decoder_input1 = nn.Linear(filters[3]*63*13, latent_dim) #for marmousi 101x101
         #self.decoder_input = nn.Linear(latent_dim, filters[3]*100*26) #for marmousi 101x101
         #self.decoder_input1 = nn.Linear(filters[1]*100*18, latent_dim) #for marmousi 101x101
-        self.decoder_input = nn.Linear(latent_dim, filters[3]*25*13) #for marmousi 101x101
+        self.decoder_input = nn.Linear(latent_dim, filters[3]*26*13) #for marmousi 101x101
         
         
         #self.up4     = autoUp(filters[4], filters[3], self.is_deconv)
@@ -4481,7 +4481,7 @@ class AutoMarmousi23_Net(nn.Module):
         #z = 0.5*torch.ones([1,1,1,64])
         z = self.decoder_input(p)
         #z = z.view(-1, filters[3], 250, 51) #for marmousi model
-        z = z.view(-1, filters[3], 13, 25)
+        z = z.view(-1, filters[3], 13, 26)
     
         up1    = self.up3(z)
         print(" shape of up3 :", np.shape(up1))
