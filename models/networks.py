62e46f2e56edad2f1fa2c576b1d7edfaaaeb3f35
Arnab Dhara <adhara@IG-A03506.local>
2021-12-10 16:00:01 -0600

    21

--- models/networks.py
+++ models/networks.py
@@ -4188,7 +4188,7 @@ class AutoMarmousi24_Net(nn.Module):
         maxtrue = torch.max(inputs1)
         meandata = torch.mean(inputs2)
         stddata = torch.std(inputs2)
-        down1  = self.down1((inputs2[:,:,1:4001:4,:])
+        down1  = self.down1((inputs2[:,:,1:4001:4,:]))
         down2  = self.down2(down1)
         down3  = self.down3(down2)
         down4  = self.down4(down3)
