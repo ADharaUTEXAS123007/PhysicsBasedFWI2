19619cc3f387c64d9d3be7500b81c3ac19f2d199
Arnab Dhara <adhara@IG-A03506.local>
2021-12-10 02:10:57 -0600

    21

--- models/Auto21_model.py
+++ models/Auto21_model.py
@@ -104,7 +104,7 @@ class Auto21Model(BaseModel):
         # specify the training losses you want to print out. The training/test scripts will call <BaseModel.get_current_losses>
         self.loss_names = ['D_MSE', 'M_MSE', 'V_MSE', 'K_MSE']
         # specify the images you want to save/display. The training/test scripts will call <BaseModel.get_current_visuals>
-        self.visual_names = ['fake_B','real_B','fake_BT', 'real_BT']
+        self.visual_names = ['fake_BT', 'real_BT']
         # specify the models you want to save to the disk. The training/test scripts will call <BaseModel.save_networks> and <BaseModel.load_networks>
         if self.isTrain:
             self.model_names = ['G']
