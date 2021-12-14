cce764a46907828c9cb9a26131759c98f19bfb1b
Arnab Dhara <adhara@IG-A03506.local>
2021-12-14 04:11:06 -0600

    vae

--- models/networks.py
+++ models/networks.py
@@ -4683,7 +4683,7 @@ class AutoMarmousi25_Net(nn.Module):
         #print("device ordinal :", self.devicek)
         source_amplitudes_true = source_amplitudes_true.to(devicek)
         #lstart = -1
-        num_batches = 2
+        num_batches = 3
         num_epochs = 1
         if (epoch1 > lstart):
             num_epochs = 1
