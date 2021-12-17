97ed9ea15cf18d7ac14ebe493f9bfda1f83ced8c
Arnab Dhara <adhara@IG-A03506.local>
2021-12-17 09:28:37 -0600

    automarmousi26

--- models/networks.py
+++ models/networks.py
@@ -4975,7 +4975,7 @@ class AutoMarmousi26_Net(nn.Module):
         up2    = self.up2(up3)
         up1    = self.up1(up2)
         print("shape of up1 :", np.shape(up1))
-        up1    = up1[:,:,1:1+label_dsp_dim[0],0:1+label_dsp_dim[1]].contiguous()
+        up1    = up1[:,:,1:1+label_dsp_dim[0],1:1+label_dsp_dim[1]].contiguous()
         f1     = self.f1(up1)
         f1     = self.final(f1)
         #f1     = self.final1(f1)
