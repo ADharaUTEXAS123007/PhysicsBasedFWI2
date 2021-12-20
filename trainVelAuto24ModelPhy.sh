a8d5a5ffbc582e651ca035c5338848b626694330
Arnab Dhara <adhara@wireless-10-147-44-38.public.utexas.edu>
2021-12-20 10:32:01 -0600

    24

--- trainVelAuto24ModelPhy.sh
+++ trainVelAuto24ModelPhy.sh
@@ -1,4 +1,4 @@
 
 mkdir ./marmousi24
 rm -rf /disk/student/adhara/Fall2021/PhysicsBasedFWI/checkpoints/MarmousiOpenFWI24/*.txt
-no_proxy=localhost python trainValLatent4dVel2.py --dataroot /disk/student/adhara/Fall2021/FCNVMB-Deep-learning-based-seismic-velocity-model-building/MarmousiF/ --name MarmousiOpenFWI24 --model Auto24 --direction AtoB --input_nc 18 --output_nc 1 --display_port 9998 --n_epochs 1500 --n_epochs_decay 1500  --batch_size 8 --gpu_ids 0 --no_html --display_freq 1 --print_freq 1 --lr 0.009 --verbose --save_epoch_freq 25 --continue_train
+no_proxy=localhost python trainValLatent4dVel2.py --dataroot /disk/student/adhara/Fall2021/FCNVMB-Deep-learning-based-seismic-velocity-model-building/MarmousiF/ --name MarmousiOpenFWI24 --model Auto24 --direction AtoB --input_nc 18 --output_nc 1 --display_port 9998 --n_epochs 1500 --n_epochs_decay 1500  --batch_size 8 --gpu_ids 0 --no_html --display_freq 1 --print_freq 1 --lr 0.005 --verbose --save_epoch_freq 25 --continue_train
