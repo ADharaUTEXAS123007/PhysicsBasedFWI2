7b3b787e9156dbbe560010fba8fd3fd50d68fa37
Arnab Dhara <adhara@wireless-10-147-44-38.public.utexas.edu>
2021-12-20 15:47:22 -0600

    22

--- models/networks.py
+++ models/networks.py
@@ -4388,8 +4388,8 @@ class AutoMarmousi22_Net(nn.Module):
     # forward modeling to compute gradients
     def prop(self, inputs, vel, lstart, epoch1, mintrue, maxtrue, true):
         
-        torch.cuda.set_device(1)  #RB Necessary if device <> 0
-        GPU_string='cuda:'+str(1)
+        torch.cuda.set_device(6)  #RB Necessary if device <> 0
+        GPU_string='cuda:'+str(6)
         devicek = torch.device(GPU_string)
         #vel = vel.to(devicek)
         #net1out1 = mintrue + vel*(maxtrue-mintrue)
--- trainVelAuto22ModelPhy.sh
+++ trainVelAuto22ModelPhy.sh
@@ -1,4 +1,4 @@
 
 mkdir ./marmousi22
 rm -rf /disk/student/adhara/Fall2021/PhysicsBasedFWI/checkpoints/MarmousiOpenFWI22/*.txt
-no_proxy=localhost python trainValLatent4dVel2.py --dataroot /disk/student/adhara/Fall2021/FCNVMB-Deep-learning-based-seismic-velocity-model-building/MarmousiN/ --name MarmousiOpenFWI22 --model Auto22 --direction AtoB --input_nc 18 --output_nc 1 --display_port 9990 --n_epochs 2500 --n_epochs_decay 2500 --batch_size 8 --gpu_ids 2 --no_html --display_freq 1 --print_freq 1 --lr 0.01 --verbose --save_epoch_freq 25
+no_proxy=localhost python trainValLatent4dVel2.py --dataroot /disk/student/adhara/Fall2021/FCNVMB-Deep-learning-based-seismic-velocity-model-building/MarmousiN/ --name MarmousiOpenFWI22 --model Auto22 --direction AtoB --input_nc 18 --output_nc 1 --display_port 9990 --n_epochs 2500 --n_epochs_decay 2500 --batch_size 8 --gpu_ids 7 --no_html --display_freq 1 --print_freq 1 --lr 0.01 --verbose --save_epoch_freq 25
