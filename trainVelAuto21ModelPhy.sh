a8cf55efe7967b37810cdad4974852c19b9e31f7
Arnab Dhara <adhara@IG-A03506.local>
2021-12-21 04:53:15 -0600

    21

--- trainVelAuto21ModelPhy.sh
+++ trainVelAuto21ModelPhy.sh
@@ -1,4 +1,4 @@
 
 mkdir ./marmousi21
 rm -rf /disk/student/adhara/Fall2021/PhysicsBasedFWI/checkpoints/SEAMOpenFWI21/*.txt
-no_proxy=localhost python trainValLatent4dVel2.py --dataroot /disk/student/adhara/Fall2021/FCNVMB-Deep-learning-based-seismic-velocity-model-building/SEAMN/ --name SEAMOpenFWI21 --model Auto21 --direction AtoB --input_nc 20 --output_nc 1 --display_port 9996 --n_epochs 2500 --n_epochs_decay 3500  --batch_size 8 --gpu_ids 5 --no_html --display_freq 1 --print_freq 1 --lr 0.01 --verbose --save_epoch_freq 25 --continue_train
+no_proxy=localhost python trainValLatent4dVel2.py --dataroot /disk/student/adhara/Fall2021/FCNVMB-Deep-learning-based-seismic-velocity-model-building/SEAMN/ --name SEAMOpenFWI21 --model Auto21 --direction AtoB --input_nc 20 --output_nc 1 --display_port 9996 --n_epochs 2500 --n_epochs_decay 3500  --batch_size 8 --gpu_ids 5 --no_html --display_freq 1 --print_freq 1 --lr 0.01 --verbose --save_epoch_freq 25
