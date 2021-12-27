8e9e9e8921555c1570f57f8a23bae75d920474a9
Arnab Dhara <adhara@IG-A03506.local>
2021-12-27 10:14:21 -0600

    1D

--- models/networks.py
+++ models/networks.py
@@ -2779,10 +2779,10 @@ class AutoMarmousi_Net(nn.Module):
         ##self.decoder_input1 = nn.Linear(filters[1]*250*51, latent_dim) #for marmousi 151x200
         #self.decoder_input1 = nn.Linear(filters[2]*125*26, latent_dim) #for marmousi 151x200
         #self.decoder_input = nn.Linear(latent_dim, filters[2]*500*102) #for marmousi 151x200
-        self.decoder_input1 = nn.Linear(filters[3]*69*43, latent_dim) #for marmousi 101x101
+        self.decoder_input1 = nn.Linear(filters[3]*69*16, latent_dim) #for marmousi 101x101
         #self.decoder_input = nn.Linear(latent_dim, filters[3]*100*26) #for marmousi 101x101
         #self.decoder_input1 = nn.Linear(filters[1]*100*18, latent_dim) #for marmousi 101x101
-        self.decoder_input = nn.Linear(latent_dim, filters[3]*138*86) #for marmousi 101x101
+        self.decoder_input = nn.Linear(latent_dim, filters[3]*138*43) #for marmousi 101x101
         
         
         #self.up4     = autoUp(filters[4], filters[3], self.is_deconv)
@@ -2799,7 +2799,7 @@ class AutoMarmousi_Net(nn.Module):
     def forward(self, inputs1, inputs2, lstart, epoch1, latentI, lowf):
         filters = [16, 32, 64, 128, 512]
         latent_dim = 8
-        label_dsp_dim = (1097,681)
+        label_dsp_dim = (1097,249)
         mintrue = torch.min(inputs1)
         maxtrue = torch.max(inputs1)
         print("shape of inputs2 :", np.shape(inputs2))
@@ -2837,7 +2837,7 @@ class AutoMarmousi_Net(nn.Module):
         #z = 0.5*torch.ones([1,1,1,64])
         z = self.decoder_input(p)
         #z = z.view(-1, filters[3], 250, 51) #for marmousi model
-        z = z.view(-1, filters[3], 138, 86)
+        z = z.view(-1, filters[3], 138, 43)
     
         up1    = self.up3(z)
         #print(" shape of up1 :", np.shape(up1))
