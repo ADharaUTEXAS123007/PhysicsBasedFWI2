8debea68042b78deea3f2152fa7712420041326b
Arnab Dhara <adhara@IG-A03506.local>
2021-12-27 12:29:16 -0600

    1D

--- models/networks.py
+++ models/networks.py
@@ -2793,8 +2793,8 @@ class AutoMarmousi_Net(nn.Module):
         #self.upff2     = autoUp(filters[0], filters[0], self.is_deconv)
         self.f1      =  nn.Conv2d(filters[0],self.n_classes, 1)
         #self.f2      =  nn.Conv2d(1,1,1)
-        #self.final   =  nn.Tanh()
-        self.final  =  nn.Sigmoid()
+        self.final   =  nn.Tanh()
+        #self.final  =  nn.Sigmoid()
         
     def forward(self, inputs1, inputs2, lstart, epoch1, latentI, lowf):
         filters = [16, 32, 64, 128, 512]
@@ -2851,12 +2851,12 @@ class AutoMarmousi_Net(nn.Module):
         #f1     = self.final(f1)
         #f1     = f1/torch.max(f1)
         #print("shape of f1 :", np.shape(f1))
-        
-        f1    = mintrue + f1*(maxtrue-mintrue)
+        f1     = lowf + 0.009*f1
+        #f1    = mintrue + f1*(maxtrue-mintrue)
         #f1[(inputs1==1.5100)] = 1.510
         #f1     = lowf + f1
         #f1[(inputs1 == 1.510)] = 1.510
-        #f1     = torch.clamp(f1,min=mintrue,max=maxtrue)
+        f1     = torch.clip(f1,min=mintrue,max=maxtrue)
         
         #f1     = torch.add(f1,1600.0)
         #f1     = torch.add(f1,lowf)
