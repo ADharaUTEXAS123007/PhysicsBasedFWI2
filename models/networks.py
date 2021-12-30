275b682e9131adb95f307c0d888f6c042ce363d0
Arnab Dhara <adhara@IG-A03506.local>
2021-12-30 16:44:32 -0600

    auto

--- models/networks.py
+++ models/networks.py
@@ -2848,7 +2848,7 @@ class AutoMarmousi_Net(nn.Module):
         up1    = up1[:,:,1:1+label_dsp_dim[0],1:1+label_dsp_dim[1]].contiguous()
         f1     = self.f1(up1)
         #f1     = self.f2(f1)
-        f1     = self.final(f1)
+        #f1     = self.final(f1)
         #f1     = self.final1(f1)
         #f1     = self.final(f1)
         #f1     = f1/torch.max(f1)
