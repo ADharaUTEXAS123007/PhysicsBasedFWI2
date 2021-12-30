a024248522dcd76fd14b7da71a3c095fc109655c
Arnab Dhara <adhara@IG-A03506.local>
2021-12-30 12:44:13 -0600

    auto

--- models/networks.py
+++ models/networks.py
@@ -2763,7 +2763,7 @@ class AutoMarmousi_Net(nn.Module):
         super(AutoMarmousi_Net, self).__init__()
         self.is_deconv     = False
         self.in_channels   = outer_nc
-        self.is_batchnorm  = True
+        self.is_batchnorm  = False
         self.n_classes     = inner_nc
         
         #filters = [16, 32, 64, 128, 512]
