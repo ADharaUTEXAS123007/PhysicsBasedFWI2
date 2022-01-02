8ec2566e9736e21c1bcecf27f46afe7063f6d6b4
Arnab Dhara <adhara@IG-A03506.local>
2022-01-02 08:49:41 -0600

    automarmousi22

--- models/networks.py
+++ models/networks.py
@@ -2145,9 +2145,11 @@ class unetConv2(nn.Module):
         # Kernel size: 3*3, Stride: 1, Padding: 1
         if is_batchnorm:
             self.conv1 = nn.Sequential(nn.Conv2d(in_size, out_size, 3, 1, 1),
+                                       nn.Dropout2d(0.3),
                                        nn.BatchNorm2d(out_size),
                                        nn.LeakyReLU(0.1))
             self.conv2 = nn.Sequential(nn.Conv2d(out_size, out_size, 3, 1, 1),
+                                       nn.Dropout2d(0.3),
                                        nn.BatchNorm2d(out_size),
                                        nn.LeakyReLU(0.1))
         else:
@@ -2350,9 +2352,11 @@ class unetConv5(nn.Module):
         # Kernel size: 3*3, Stride: 1, Padding: 1
         if is_batchnorm:
             self.conv1 = nn.Sequential(nn.Conv2d(in_size, out_size, 3, 1, 1),
+                                       nn.Dropout2d(0.3),
                                        nn.BatchNorm2d(out_size),
                                        nn.LeakyReLU(0.1))
             self.conv2 = nn.Sequential(nn.Conv2d(out_size, out_size, 3, 1, 1),
+                                       nn.Dropout2d(0.3),
                                        nn.BatchNorm2d(out_size),
                                        nn.LeakyReLU(0.1))
         else:
