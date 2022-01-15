73ccf7dca9f1ba4a110fcb93ae8df9cce77d0c32
Arnab Dhara <adhara@IG-A03506.local>
2022-01-15 10:25:03 -0600

    fwi

--- models/networks.py
+++ models/networks.py
@@ -3130,8 +3130,8 @@ class UnetMarmousi_Net(nn.Module):
         self.is_batchnorm  = True
         self.n_classes     = 1
         
-        #filters = [16, 32, 64, 128, 256]
-        filters = [2, 4, 8, 16, 32]
+        filters = [32, 64, 128, 256, 512]
+        #filters = [2, 4, 8, 16, 32]
         
         latent_dim = 512
 
