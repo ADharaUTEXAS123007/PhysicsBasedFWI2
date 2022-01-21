f7e781193274e8e7e323e90912e82467fc1bd46c
Arnab Dhara <adhara@IG-A03506.local>
2022-01-21 13:10:12 -0600

    elastic

--- models/networks.py
+++ models/networks.py
@@ -5055,9 +5055,9 @@ class AutoElMarmousi22_Net(nn.Module):
         #f1     = self.final1(f1)
         #f1     = self.final(f1)
         #f1     = f1/torch.max(f1)
-        #print("shape of f1 :", np.shape(f1))
-        print("mintrue :", mintrue)
-        print("maxtrue :", maxtrue)
+        print("shape of f1 :", np.shape(f1))
+        #print("mintrue :", mintrue)
+        #print("maxtrue :", maxtrue)
         
         #f1    = mintrue + f1*(maxtrue-mintrue)
         #f1[(inputs1==1500)] = 1500
