2d18f20e9008b9617a4b9c434223534d9c3458d2
Arnab Dhara <adhara@IG-A03506.local>
2022-01-21 12:51:27 -0600

    elastic

--- models/networks.py
+++ models/networks.py
@@ -4970,7 +4970,7 @@ class AutoElMarmousi22_Net(nn.Module):
         ##self.decoder_input1 = nn.Linear(filters[1]*250*51, latent_dim) #for marmousi 151x200
         #self.decoder_input1 = nn.Linear(filters[2]*125*26, latent_dim) #for marmousi 151x200
         #self.decoder_input = nn.Linear(latent_dim, filters[2]*500*102) #for marmousi 151x200
-        self.decoder_input1 = nn.Linear(filters[3]*63*13, latent_dim) #for marmousi 101x101
+        self.decoder_input1 = nn.Linear(filters[3]*19*14, latent_dim) #for marmousi 101x101
         #self.decoder_input = nn.Linear(latent_dim, filters[3]*100*26) #for marmousi 101x101
         #self.decoder_input1 = nn.Linear(filters[1]*100*18, latent_dim) #for marmousi 101x101
         self.decoder_input = nn.Linear(latent_dim, filters[3]*25*19) #for marmousi 101x101
