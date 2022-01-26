8e82f7e2e095681c2174a3984fa895810d64ee14
Arnab Dhara <adhara@IG-A03506.local>
2022-01-26 13:15:32 -0600

    el22

--- util/visualizer.py
+++ util/visualizer.py
@@ -56,7 +56,7 @@ def save_matrix(visuals, image_path, mnumber, aspect_ratio=1.0, width=256):
 
     This function will save images stored in 'visuals' to the HTML file specified by 'webpage'.
     """
-    image_dir = '/disk/student/adhara/Fall2021/ImpedanceOutput/Output' + str(mnumber)
+    image_dir = '/disk/student/adhara/Fall2021/FCNVMB-Deep-learning-based-seismic-velocity-model-building/Output' + str(mnumber)
     short_path = ntpath.basename(image_path[0])
     name = os.path.splitext(short_path)[0]
 
