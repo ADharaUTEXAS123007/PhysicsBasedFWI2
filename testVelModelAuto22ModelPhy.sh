568c9996fa2fa7a5f5fb78c5c8ba2340d052d593
Arnab Dhara <adhara@IG-A03506.local>
2022-01-27 05:50:03 -0600

    2200

--- testVelModelAuto22ModelPhy.sh
+++ testVelModelAuto22ModelPhy.sh
@@ -1,2 +1,2 @@
 
-python test4d.py --dataroot /disk/student/adhara/Fall2021/FCNVMB-Deep-learning-based-seismic-velocity-model-building/MarmousiN/ --name MarmousiOpenFWI22 --model Auto22 --direction AtoB --input_nc 18 --output_nc 1 --realization 35 --epoch 1350 --gpu_ids 5
+python test4d.py --dataroot /disk/student/adhara/Fall2021/FCNVMB-Deep-learning-based-seismic-velocity-model-building/MarmousiN/ --name MarmousiOpenFWI22 --model Auto22 --direction AtoB --input_nc 18 --output_nc 1 --realization 35 --epoch 2200 --gpu_ids 5
