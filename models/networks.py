231fb4e75b23a6a3b80658cd930603438168ff45
Arnab Dhara <adhara@IG-A03506.local>
2022-01-26 21:53:40 -0600

    marmousi2

--- models/networks.py
+++ models/networks.py
@@ -5327,13 +5327,13 @@ class AutoElMarmousi22_Net(nn.Module):
         
         vp_grad = torch.from_numpy(vp_grad.copy())
         vp_grad = vp_grad.float()
-        vp_grad = vp_grad*(10**0)
+        vp_grad = vp_grad*(10**5)
         vs_grad = torch.from_numpy(vs_grad.copy())
         vs_grad = vs_grad.float()
-        vs_grad = vs_grad*(10**0)
+        vs_grad = vs_grad*(10**5)
         rho_grad = torch.from_numpy(rho_grad.copy())
         rho_grad = rho_grad.float()
-        rho_grad = rho_grad*(10**0)
+        rho_grad = rho_grad*(10**5)
         
         print('grads names')
         print(fnames)
