e9562210307c16185598bba25a124ec73d6567f6
Arnab Dhara <adhara@IG-A03506.local>
2022-01-28 11:00:20 -0600

    seis1

--- models/AutoEl22_model.py
+++ models/AutoEl22_model.py
@@ -160,8 +160,8 @@ class AutoEl22Model(BaseModel):
         self.vs_grad = torch.unsqueeze(self.vs_grad,0)
         self.rho_grad = torch.unsqueeze(self.rho_grad,0)
         
-        self.grad = torch.cat((self.vp_grad,self.vs_grad,self.rho_grad),dim=0)
-        #self.grad = self.vs_grad
+        #self.grad = torch.cat((self.vp_grad,self.vs_grad,self.rho_grad),dim=0)
+        self.grad = self.vp_grad
         self.grad = torch.unsqueeze(self.grad,0)
         #self.latent = self.latent.clone().detach()
         #print("self.latent :", self.latent)
