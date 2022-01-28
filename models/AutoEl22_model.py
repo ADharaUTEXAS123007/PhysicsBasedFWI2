e53afac73d535526f5a53d2c098dd73d2b9c0d50
Arnab Dhara <adhara@IG-A03506.local>
2022-01-28 10:59:14 -0600

    seis1

--- models/AutoEl22_model.py
+++ models/AutoEl22_model.py
@@ -153,8 +153,8 @@ class AutoEl22Model(BaseModel):
         self.real_Rho = torch.unsqueeze(self.real_B[:,2,:,:],1)
         
         self.fake_Vp = torch.unsqueeze(self.fake_B[:,0,:,:],1)
-        self.fake_Vs = torch.unsqueeze(self.fake_B[:,1,:,:],1)
-        self.fake_Rho = torch.unsqueeze(self.fake_B[:,2,:,:],1)
+        self.fake_Vs = torch.unsqueeze(self.real_B[:,1,:,:],1)
+        self.fake_Rho = torch.unsqueeze(self.real_B[:,2,:,:],1)
         
         self.vp_grad = torch.unsqueeze(self.vp_grad,0)
         self.vs_grad = torch.unsqueeze(self.vs_grad,0)
