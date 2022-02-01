64360d9b9a758f72dadd504d7b375ff731fffbb3
Arnab Dhara <adhara@IG-A03506.local>
2022-02-01 11:05:57 -0600

    lbfgs

--- models/AutoEl22_model.py
+++ models/AutoEl22_model.py
@@ -409,7 +409,7 @@ class AutoEl22Model(BaseModel):
         self.optimizer_G.zero_grad()
         self.backward_G11(epoch,batch,lstart)
         print("loss type")
-        loss = np.
+        #loss = np.
         loss = torch.from_numpy(loss)
         loss = loss.float()
         loss = torch.unsqueeze(loss,1)
