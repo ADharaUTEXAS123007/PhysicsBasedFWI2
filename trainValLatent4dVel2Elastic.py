1daccc82e913f86c87852b270f236249d37c6bc5
Arnab Dhara <adhara@IG-A03506.local>
2022-02-01 20:35:51 -0600

    Lhist

--- trainValLatent4dVel2Elastic.py
+++ trainValLatent4dVel2Elastic.py
@@ -47,6 +47,7 @@ if __name__ == '__main__':
     losses1 = OrderedDict()
     lstart = 0
     Lhist = np.zeros(4)
+    freqL = [3,5,8,10,14]
     for epoch in range(opt.epoch_count, opt.n_epochs + opt.n_epochs_decay + 1):    # outer loop for different epochs; we save the model by <epoch_count>, <epoch_count>+<save_latest_freq>
          epoch_start_time = time.time()  # timer for entire epoch
          iter_data_time = time.time()    # timer for data loading per iteration
@@ -116,7 +117,10 @@ if __name__ == '__main__':
              if (epoch < 4):
                  Lhist[epoch-1] = model.loss_D_MSE
              else:
+                 Lhist[1] = Lhist[2]
+                 Lhist[2] = Lhist[3]
                  Lhist[3] = model.loss_D_MSE
+                 
              #if (epoch > lstart):
              #   Model1loss = Model1loss + model.loss_M1_MSE.item()     
              #else:
