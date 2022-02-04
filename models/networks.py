00fb2f1c2a3f5450ab040bd2c3ce45498a454576
Arnab Dhara <adhara@IG-A03506.local>
2022-02-04 16:17:09 -0600

    2200

--- models/networks.py
+++ models/networks.py
@@ -5675,11 +5675,11 @@ class AutoElMarmousi22_Net(nn.Module):
         d.NPROCX = 6
         d.NPROCY = 6
         d.PHYSICS = 1
-        d.VPUPPERLIM = 2000.0
+        d.VPUPPERLIM = 3000.0
         d.VPLOWERLIM = 1500.0
-        d.VSUPPERLIM = 1155.0
+        d.VSUPPERLIM = 1732.0
         d.VSLOWERLIM = 866.0
-        d.RHOUPPERLIM = 2073.0
+        d.RHOUPPERLIM = 2294.0
         d.RHOLOWERLIM = 1929.0
         #d.forward(model, src, rec)
         #os.system('mpirun -np 4 hello')
