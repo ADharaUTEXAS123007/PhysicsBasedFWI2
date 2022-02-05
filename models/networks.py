05a2ada47035ad21c24873577e5e7bb235529407
Arnab Dhara <adhara@IG-A03506.local>
2022-02-05 08:07:46 -0600

    fwi

--- models/networks.py
+++ models/networks.py
@@ -5495,13 +5495,13 @@ class AutoElMarmousi22_Net(nn.Module):
         ###vs1    = self.vs(torch.unsqueeze(f1[:,1,:,:],1))
         #rho1   = self.rho(f13)
         
-        #vp1    = self.final1(vp1)
-        #vs1    = self.final2(vs1)
+        vp1    = self.final1(vp1)
+        vs1    = self.final2(vs1)
         #rho1   = self.final(rho1)
         
         
-        #vp1    = torch.unsqueeze(lowf[:,0,:,:],1) + vp1
-        #vs1    = torch.unsqueeze(lowf[:,1,:,:],1) + vs1
+        vp1    = torch.unsqueeze(lowf[:,0,:,:],1) + vp1
+        vs1    = torch.unsqueeze(lowf[:,1,:,:],1) + vs1
         #rho1   = torch.unsqueeze(lowf[:,2,:,:],1) + rho1
         
         #vp1     = self.final1(vp1)
