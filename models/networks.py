d03e5869dd9904d7dd4de94121226039646fd074
Arnab Dhara <adhara@IG-A03506.local>
2022-02-06 00:24:03 -0600

    fwi

--- models/networks.py
+++ models/networks.py
@@ -5354,7 +5354,7 @@ class AutoElMarmousi22_Net(nn.Module):
         self.decoder_input1 = nn.Linear(filters[3]*38*14, latent_dim) #for marmousi 101x101
         #self.decoder_input = nn.Linear(latent_dim, filters[3]*100*26) #for marmousi 101x101
         #self.decoder_input1 = nn.Linear(filters[1]*100*18, latent_dim) #for marmousi 101x101
-        self.decoder_input = nn.Linear(latent_dim, filters[3]*38*19) #for marmousi 101x101
+        self.decoder_input = nn.Linear(latent_dim, filters[3]*19*9) #for marmousi 101x101
         
         
         #self.up4     = autoUp(filters[4], filters[3], self.is_deconv)
