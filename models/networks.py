7d2b4271ffe5edece244fcb9457432d7f86a2474
Arnab Dhara <adhara@IG-A03506.local>
2022-02-08 21:35:14 -0600

    po

--- models/networks.py
+++ models/networks.py
@@ -5355,7 +5355,7 @@ class AutoElMarmousi22_Net(nn.Module):
         self.decoder_input1 = nn.Linear(filters[3]*38*14, latent_dim) #for marmousi 101x101
         #self.decoder_input = nn.Linear(latent_dim, filters[3]*100*26) #for marmousi 101x101
         #self.decoder_input1 = nn.Linear(filters[1]*100*18, latent_dim) #for marmousi 101x101
-        self.decoder_input = nn.Linear(latent_dim, filters[3]*38*19) #for marmousi 101x101
+        self.decoder_input = nn.Linear(latent_dim, filters[3]*38*20) #for marmousi 101x101
         
         
         #self.up4     = autoUp(filters[4], filters[3], self.is_deconv)
@@ -5459,7 +5459,7 @@ class AutoElMarmousi22_Net(nn.Module):
         z = self.decoder_input(p)
         #####z = inputs2
         #z = z.view(-1, filters[3], 250, 51) #for marmousi model
-        z = z.view(-1, filters[3], 19, 38)
+        z = z.view(-1, filters[3], 20, 38)
     
         #up31    = self.up31(z)
         #up32    = self.up32(z)
@@ -5511,9 +5511,9 @@ class AutoElMarmousi22_Net(nn.Module):
         vs1    = torch.unsqueeze(lowf[:,1,:,:],1) + vs1
         rho1   = torch.unsqueeze(lowf[:,2,:,:],1) + rho1
         
-        vp1[:,:,0:6,:] = inputs1[:,0,0:6,:]
-        vs1[:,:,0:6,:] = inputs1[:,1,0:6,:]
-        rho1[:,:,0:6,:] = inputs1[:,2,0:6,:]
+        vp1[:,:,0:12,:] = inputs1[:,0,0:12,:]
+        vs1[:,:,0:12,:] = inputs1[:,1,0:12,:]
+        rho1[:,:,0:12,:] = inputs1[:,2,0:12,:]
         
         #vp1     = self.final1(vp1)
         #vs1     = self.final2(vs1)
