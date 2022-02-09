dd42cc7642cd5aca24ede8abdc31403426479a19
Arnab Dhara <adhara@IG-A03506.local>
2022-02-08 21:47:47 -0600

    po

--- trainValLatent4dVel2Elastic.py
+++ trainValLatent4dVel2Elastic.py
@@ -47,7 +47,7 @@ if __name__ == '__main__':
     losses1 = OrderedDict()
     lstart = 0
     Lhist = np.ones(5)
-    freqL = [20]
+    freqL = [10]
     mop = 0
     for epoch in range(opt.epoch_count, opt.n_epochs + opt.n_epochs_decay + 1):    # outer loop for different epochs; we save the model by <epoch_count>, <epoch_count>+<save_latest_freq>
          epoch_start_time = time.time()  # timer for entire epoch
