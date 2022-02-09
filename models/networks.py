05a9c95969dd128fdbf9855f140f9dc02f642958
Arnab Dhara <adhara@IG-A03506.local>
2022-02-09 12:31:28 -0600

    po

--- models/networks.py
+++ models/networks.py
@@ -5769,16 +5769,16 @@ class AutoElMarmousi22_Net(nn.Module):
         
         #print("loss :", loss)
         
-        grads, fnames = d.get_fwi_gradients(['c','old'],return_filenames=True)
+        #grads, fnames = d.get_fwi_gradients(['c','old'],return_filenames=True)
         
         #print("shape of grads :", np.shape(grads))
-        vp_grad = np.array(grads[0])
+        #vp_grad = np.array(grads[0])
+        #vs_grad = np.array(grads[2])
+        #rho_grad = np.array(grads[1])
+        grads, fnames = d.get_fwi_gradients(['seis'],return_filenames=True)
+        vp_grad = np.array(grads[1])
         vs_grad = np.array(grads[2])
-        rho_grad = np.array(grads[1])
-        ###grads, fnames = d.get_fwi_gradients(['seis'],return_filenames=True)
-        ###vp_grad = np.array(grads[1])
-        ###vs_grad = np.array(grads[2])
-        ###rho_grad = np.array(grads[0])
+        rho_grad = np.array(grads[0])
         
         print("shape of vp_grad :", np.shape(vp_grad))
         print("shape of vs_grad :", np.shape(vs_grad))
