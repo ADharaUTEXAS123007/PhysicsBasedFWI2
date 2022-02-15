1ffae73c290e3e70d10a063dad9a3ece96ccc7cf
Arnab Dhara <adhara@IG-A03506.local>
2022-02-14 23:53:42 -0600

    po

--- models/networks.py
+++ models/networks.py
@@ -5717,8 +5717,8 @@ class AutoElMarmousi22_Net(nn.Module):
         d.VSLOWERLIM = 866.0
         d.RHOUPPERLIM = 750.0
         d.RHOLOWERLIM = 2250.0
-        d.SWS_TAPER_GRAD_HOR = 1
-        d.EXP_TAPER_GRAD_HOR = 2.0
+        d.SWS_TAPER_GRAD_HOR = 0
+        #d.EXP_TAPER_GRAD_HOR = 2.0
         #d.forward(model, src, rec)
         #os.system('mpirun -np 4 hello')
         filen = './marmousiEl/vpmod' + str(epoch1) + '.npy' #switch on for physics based fwi         
