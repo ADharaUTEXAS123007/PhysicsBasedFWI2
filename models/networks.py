3d9a44055b727c3ea0fe9840a31385b0f1d9271e
Arnab Dhara <adhara@IG-A03506.local>
2022-02-21 08:06:37 -0600

    xtalk

--- models/networks.py
+++ models/networks.py
@@ -6068,8 +6068,8 @@ class AutoElMarmousiMar22_Net(nn.Module):
         vs1    = torch.unsqueeze(lowf[:,1,:,:],1) + vs1
         #rho1   = torch.unsqueeze(lowf[:,2,:,:],1) + rho1
         
-        vp1[:,:,0:22,:] = inputs1[:,0,0:25,:]
-        vs1[:,:,0:22,:] = inputs1[:,1,0:25,:]
+        vp1[:,:,0:25,:] = inputs1[:,0,0:25,:]
+        vs1[:,:,0:25,:] = inputs1[:,1,0:25,:]
         #rho1[:,:,0:22,:] = inputs1[:,2,0:22,:]
         
         #vp1     = self.final1(vp1)
