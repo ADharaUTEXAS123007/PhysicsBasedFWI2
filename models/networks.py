525216099fa6ff918b4a904b88a318f34e7fa780
Arnab Dhara <adhara@IG-A03506.local>
2022-02-23 11:00:44 -0600

    cth

--- models/networks.py
+++ models/networks.py
@@ -5386,8 +5386,8 @@ class AutoElMarmousi22_Net(nn.Module):
         self.f12      =  nn.Conv2d(filters[0],filters[0], 1)
         self.f13      =  nn.Conv2d(int(filters[0]/4), 1, 1)
         
-        self.vp     =   nn.Conv2d(int(filters[0]/4),1,1)
-        self.vs     =   nn.Conv2d(int(filters[0]/4),1,1)
+        self.vp     =   nn.Conv2d(int(filters[0]),1,1)
+        self.vs     =   nn.Conv2d(int(filters[0]),1,1)
         self.rho1    =   nn.Conv2d(1,1,1)
         
         
