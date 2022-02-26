079b328a29912dfa2ae4e78fb3e22cebd9c3f595
Arnab Dhara <adhara@IG-A03506.local>
2022-02-26 13:07:31 -0600

    cth

--- models/networks.py
+++ models/networks.py
@@ -6297,7 +6297,7 @@ class AutoElMarmousiMar22_Net(nn.Module):
         print("min max vsst :", np.min(vsst), np.max(vsst))
         print("min max rhost :", np.min(rhost), np.max(rhost))
         
-        model_init = api.Model(vpst, vsst, rho, dx)
+        model_init = api.Model(vpst, vsst, rhost, dx)
         
         
         d.fwi_stages = []
