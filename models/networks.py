03218cef2136350ba345551b08bb6a46c9d0dd0a
Arnab Dhara <adhara@IG-A03506.local>
2022-02-26 12:54:06 -0600

    cth

--- models/networks.py
+++ models/networks.py
@@ -5927,7 +5927,7 @@ class AutoElMarmousiMar22_Net(nn.Module):
         #self.dropU2  = nn.Dropout2d(0.025)
         self.up11     = autoUp5(filters[1], filters[0], self.is_deconv)
         self.up12     = autoUp5(filters[1], filters[0], self.is_deconv)
-        self.up13     = autoUp5(int(filters[1]/4), int(filters[0]/4), self.is_deconv)
+        self.up13     = autoUp5(int(filters[1]), int(filters[0]/4), self.is_deconv)
         #self.up1     = autoUp5(filters[1], filters[0], self.is_deconv)
         #self.dropU1  = nn.Dropout2d(0.025)
         ###self.upff1     = autoUp(filters[0], filters[0], self.is_deconv)
