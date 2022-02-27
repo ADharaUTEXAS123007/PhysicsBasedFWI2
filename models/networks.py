351c3218e2755a4513f073902a6f600cf3e6384e
Arnab Dhara <adhara@IG-A03506.local>
2022-02-27 08:55:27 -0600

    cth

--- models/networks.py
+++ models/networks.py
@@ -5884,11 +5884,11 @@ class AutoElMarmousiMar22_Net(nn.Module):
         self.n_classes     = 1
         
         #filters = [16, 32, 64, 128, 256]
-        filters = [32, 64, 128, 256, 512]
+        #filters = [32, 64, 128, 256, 512]
         #filters = [16, 32, 64, 128, 512]
         #filters = [2, 4, 8, 16, 32] #this works best result so far for marmousi model
         #filters = [1, 1, 2, 4, 16]
-        #filters = [8, 16, 32, 64, 128] 
+        filters = [8, 16, 32, 64, 128] 
         ####filters = [4, 8, 16, 32, 64]
         #########filters = [2, 4, 8, 16, 32]
         
@@ -5954,9 +5954,9 @@ class AutoElMarmousiMar22_Net(nn.Module):
         #filters = [16, 32, 64, 128, 256]
         ########filters = [2, 4, 8, 16, 32]
         #filters = [4,8,16,32]
-        ########filters = [8, 16, 32, 64, 128]  ###this works very well
+        filters = [8, 16, 32, 64, 128]  ###this works very well
         #filters = [1, 1, 2, 4, 16]
-        filters = [32, 64, 128, 256, 512]
+        #filters = [32, 64, 128, 256, 512]
         #filters = [4, 8, 16, 32, 64]
         latent_dim = 8
         label_dsp_dim = (100,300)
