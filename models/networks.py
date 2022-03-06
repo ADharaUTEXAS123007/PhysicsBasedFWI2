15726b83b24fcd24573fed0caf6525543a5bb766
Arnab Dhara <adhara@IG-A03506.local>
2022-03-06 15:24:51 -0600

    200

--- models/networks.py
+++ models/networks.py
@@ -5424,8 +5424,8 @@ class AutoElMarmousi22_Net(nn.Module):
         
         #meandata = torch.mean(inputs2)
         #stddata = torch.std(inputs2)
-        combine1 = self.combine1((inputs2[:,:,1:2400:4,:]))
-        combine2 = self.combine2((inputs3[:,:,1:2400:4,:]))
+        combine1 = self.combine1((inputs2[:,:,1:5000:5,:]))
+        combine2 = self.combine2((inputs3[:,:,1:5000:5,:]))
         
         c1c2 = torch.cat((combine1,combine2),axis=1)
         
