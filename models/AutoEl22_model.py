ee5f0e15f73c012047dc7bb0820228981dc6d503
Arnab Dhara <adhara@IG-A03506.local>
2022-03-07 11:17:50 -0600

    p

--- models/AutoEl22_model.py
+++ models/AutoEl22_model.py
@@ -413,11 +413,11 @@ class AutoEl22Model(BaseModel):
             #self.fake_Rho.retain_grad()
             #print("currenterror :", currenterror)
             #print("initerror :", initerror)
-            if (currenterror < 0.4*initerror):
+            #if (currenterror < 0.4*initerror):
                 #print("backpropagating density gradient")
-                self.rho_grad = torch.unsqueeze(self.rho_grad,0)
-                self.rho_grad = self.rho_grad.cuda(self.fake_Rho.get_device())
-                self.fake_Rho.backward(self.rho_grad)
+            self.rho_grad = torch.unsqueeze(self.rho_grad,0)
+            self.rho_grad = self.rho_grad.cuda(self.fake_Rho.get_device())
+            self.fake_Rho.backward(self.rho_grad)
                 
             #self.fake_Rho.retain_grad()
 
