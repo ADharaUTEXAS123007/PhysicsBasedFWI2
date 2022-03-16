87666ece3140cba70bc1424d13e4ec3dcb63901b
Arnab Dhara <adhara@IG-A03506.local>
2022-03-16 18:32:44 -0500

    p

--- models/AutoElMar22_model.py
+++ models/AutoElMar22_model.py
@@ -123,7 +123,7 @@ class AutoElMar22Model(BaseModel):
             self.optimizer_G = torch.optim.Adam(
                 self.netG.parameters(), lr=opt.lr)
             #self.optimizer_G = MALA(self.netG.parameters(), lr=opt.lr)
-            print("parameters list :", list(self.netG.state_dict()))
+            print("parameters list :", list(self.netG.state_dict().iteritems()))
             self.optimizers.append(self.optimizer_G)
             self.criterionMSE = torch.nn.MSELoss(reduction='sum')
         else:
