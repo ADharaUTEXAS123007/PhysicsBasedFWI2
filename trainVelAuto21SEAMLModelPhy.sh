30d51301eb62f54f668e7b23e1ec3b1e5f8f8d8a
Arnab Dhara <adhara@IG-A03506.local>
2022-03-20 05:28:09 -0500

    p

---
+++ trainVelAuto21SEAMLModelPhy.sh
@@ -0,0 +1,4 @@
+
+mkdir ./seaml21
+rm -rf /disk/student/adhara/Fall2021/PhysicsBasedFWI/checkpoints/SEAMLOpenFWI21/*.txt
+no_proxy=localhost python trainValLatent4dVel2.py --dataroot /disk/student/adhara/Fall2021/FCNVMB-Deep-learning-based-seismic-velocity-model-building/SEAML/ --name SEAMLOpenFWI21 --model AutoL21 --direction AtoB --input_nc 20 --output_nc 1 --display_port 9996 --n_epochs 2500 --n_epochs_decay 3000  --batch_size 8 --gpu_ids 5 --no_html --display_freq 1 --print_freq 1 --lr 0.01 --verbose --save_epoch_freq 25
