1343ad4a6710d6f995ee814bdec0a1b017f60e30
Arnab Dhara <adhara@IG-A03506.local>
2022-03-20 05:36:55 -0500

    p

--- models/networks.py
+++ models/networks.py
@@ -4567,7 +4567,7 @@ class AutoMarmousi21_Net(nn.Module):
                     #y_pred3 = vgg(torch.unsqueeze(torch.swapaxes(batch_rcv_amps_pred_norm[:,6:9,:],0,1),0))
                     
                     #lossinner2 = criterion1(y_pred1,y_true1) + criterion1(y_pred2,y_true2) + criterion1(y_pred3,y_true3)
-                    lossinner = lossinner1/0.208
+                    lossinner = lossinner1
                     
                     ####y_c_features = vgg(torch.unsqueeze(torch.swapaxes(batch_rcv_amps_true[:,0:3,:],0,1),0))
                     #########model2.grad[0:26,:] = 0
