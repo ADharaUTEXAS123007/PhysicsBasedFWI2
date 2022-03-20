8661f8e735f826f864b3682dbd0b986dd32467b6
Arnab Dhara <adhara@IG-A03506.local>
2022-03-20 13:39:41 -0500

    p

--- models/AutoElMar22_model.py
+++ models/AutoElMar22_model.py
@@ -436,11 +436,11 @@ class AutoElMar22Model(BaseModel):
             #self.fake_Rho.retain_grad()
             #print("currenterror :", currenterror)
             #print("initerror :", initerror)
-            #if (currenterror < 0.5*initerror):
+            if (currenterror < 0.5*initerror):
                 #print("backpropagating density gradient")
-            self.rho_grad = torch.unsqueeze(self.rho_grad,0)
-            self.rho_grad = self.rho_grad.cuda(self.fake_Rho.get_device())
-            self.fake_Rho.backward(self.rho_grad)
+                self.rho_grad = torch.unsqueeze(self.rho_grad,0)
+                self.rho_grad = self.rho_grad.cuda(self.fake_Rho.get_device())
+                self.fake_Rho.backward(self.rho_grad)
                 
             #self.fake_Rho.retain_grad()
 
