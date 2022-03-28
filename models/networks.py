3e008c7395e8892fcd1b2ee41a59c0aa261afcb5
Arnab Dhara <adhara@IG-A03506.local>
2022-03-28 10:24:44 -0500

    d

--- models/networks.py
+++ models/networks.py
@@ -6083,7 +6083,7 @@ class AutoElMarmousiMar22_Net(nn.Module):
         #rho1[:,:,0:15,:] = 0
         #rho1 = self.final3(rho1)
         
-        vp1    = torch.unsqueeze(lowf[:,0,:,:],1) + vp1
+        vp1    = torch.unsqueeze(lowf[:,0,:,:],1) - vp1
         vs1    = torch.unsqueeze(lowf[:,1,:,:],1) - vs1
         rho1   = torch.unsqueeze(lowf[:,2,:,:],1)*1 - rho1
         
