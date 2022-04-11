00f70fee052f39d5f81bb5a6e373d06e357b86e1
Arnab Dhara <adhara@IG-A03506.local>
2022-04-10 20:45:57 -0500

    AutoEl22N

--- models/networks.py
+++ models/networks.py
@@ -204,7 +204,7 @@ def define_G(input_nc, output_nc, ngf, netG, norm='batch', use_dropout=False, in
         net = AutoElMarmousiMar22_Net(input_nc, output_nc, 6, ngf, norm_layer=norm_layer, use_dropout=use_dropout)
     elif netG == 'AutoEl22N':
         print("gpu ids :", gpu_ids)
-        net = AutoElMarmousi22N_Net(input_nc, output_nc, 6, ngf, norm_layer=norm_layer, use_dropout=use_dropout, gpu_ids=gpu_ids)
+        net = AutoElMarmousi22N_Net(input_nc, output_nc, 6, ngf, norm_layer=norm_layer, use_dropout=use_dropout)
     elif netG == 'Auto23':
         net = AutoMarmousi23_Net(input_nc, output_nc, 6, ngf, norm_layer=norm_layer, use_dropout=use_dropout)
     elif netG == 'Auto24':
@@ -5907,8 +5907,8 @@ class AutoElMarmousi22_Net(nn.Module):
         return vp_grad, vs_grad, rho_grad, loss                 
     
 class AutoElMarmousi22N_Net(nn.Module):
-    def __init__(self,outer_nc, inner_nc, input_nc=None,
-                 submodule=None, outermost=False, innermost=False, norm_layer=nn.BatchNorm2d, use_dropout=False, gpu_ids):
+    def __init__(self, outer_nc, inner_nc, input_nc=None,
+                 submodule=None, outermost=False, innermost=False, norm_layer=nn.BatchNorm2d, use_dropout=False):
         super(AutoElMarmousi22N_Net, self).__init__()
         self.is_deconv     = False
         self.in_channels   = outer_nc
