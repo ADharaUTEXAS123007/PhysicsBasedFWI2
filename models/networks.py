603af2a61f06044df5593cd5549658c72043b9d5
Arnab Dhara <adhara@IG-A03506.local>
2022-04-14 16:12:11 -0500

    elastic

--- models/networks.py
+++ models/networks.py
@@ -6529,9 +6529,9 @@ class AutoElMarmousiMar22_Net(nn.Module):
         #rho1[:,:,0:15,:] = 0
         #rho1 = self.final3(rho1)
         
-        vp1    = torch.unsqueeze(lowf[:,0,:,:],1) - vp1
-        vs1    = torch.unsqueeze(lowf[:,1,:,:],1) - vs1
-        rho1   = torch.unsqueeze(lowf[:,2,:,:],1)*1 - rho1
+        vp1    = torch.unsqueeze(lowf[:,0,:,:],1) + vp1
+        vs1    = torch.unsqueeze(lowf[:,1,:,:],1) + vs1
+        rho1   = torch.unsqueeze(lowf[:,2,:,:],1)*1 + rho1
         
        
         #vp1     = self.final1(vp1)
