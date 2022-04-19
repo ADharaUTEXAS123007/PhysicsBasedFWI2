d7c1b35b5e64c97187aa43f3972e6badadb9d28e
Arnab Dhara <adhara@IG-A03506.local>
2022-04-18 22:48:18 -0500

    VelAutoElMar

--- functions/LBFGS.py
+++ functions/LBFGS.py
@@ -504,7 +504,7 @@ class LBFGS(Optimizer):
         dtype = group['dtype']
         debug = group['debug']
         
-        lr = .000
+        lr = .0005
         print("lrlrlrlrl :", lr)
 
         # variables cached in state (for tracing)
