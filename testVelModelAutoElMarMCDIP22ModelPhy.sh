1834270510078d49a435b34b683590db68dca6a1
Arnab Dhara <adhara@IG-A03506.local>
2022-04-19 17:42:55 -0500

    VelAutoElMar

--- testVelModelAutoElMarMCDIP22ModelPhy.sh
+++ testVelModelAutoElMarMCDIP22ModelPhy.sh
@@ -1,2 +1,2 @@
 
-python test4d.py --dataroot /disk/student/adhara/Fall2021/FCNVMB-Deep-learning-based-seismic-velocity-model-building/MarmousiEl/ --name MarmousiElMarMCDIP22 --model AutoElMarMCDIP22 --direction AtoB --input_nc 35 --output_nc 1 --realization 10 --epoch 2000 --gpu_ids 1
+python test4d.py --dataroot /disk/student/adhara/Fall2021/FCNVMB-Deep-learning-based-seismic-velocity-model-building/MarmousiEl/ --name MarmousiElMarMCDIP22 --model AutoElMarMCDIP22 --direction AtoB --input_nc 35 --output_nc 1 --realization 100 --epoch 2000 --gpu_ids 1
