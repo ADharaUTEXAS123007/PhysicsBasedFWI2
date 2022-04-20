7ede0209d9ba4e7078b65a2fce75620a6c90e60d
Arnab Dhara <adhara@IG-A03506.local>
2022-04-20 18:55:43 -0500

    fwi spring

--- functions/LBFGS.py
+++ functions/LBFGS.py
@@ -504,7 +504,7 @@ class LBFGS(Optimizer):
         dtype = group['dtype']
         debug = group['debug']
         
-        lr = 1.0
+        lr = 0.05
         print("lrlrlrlrl :", lr)
 
         # variables cached in state (for tracing)
