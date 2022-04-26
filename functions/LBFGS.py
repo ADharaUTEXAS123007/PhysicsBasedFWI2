d30fd656354a41f2111b53ed61e413a8105c0187
Arnab Dhara <adhara@IG-A03506.local>
2022-04-26 08:23:02 -0500

    LBFGS

--- functions/LBFGS.py
+++ functions/LBFGS.py
@@ -797,7 +797,7 @@ class LBFGS(Optimizer):
                 current_params = self._copy_params()
 
             # update and evaluate at new point
-            self._add_update(t, d)
+            self._add_update(0.0005, d)
             F_new = closure()
             closure_eval += 1
 
