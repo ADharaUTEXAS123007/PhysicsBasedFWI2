9813573594543c283c44d5ae52cdce3c8f27ea58
Arnab Dhara <adhara@utexas.edu>
2022-05-13 21:09:11 -0500

    marm

--- models/AutoElMar22_model.py
+++ models/AutoElMar22_model.py
@@ -214,7 +214,8 @@ class AutoElMar22Model(BaseModel):
         self.vs_grad = torch.unsqueeze(self.vs_grad,0)
         self.rho_grad = torch.unsqueeze(self.rho_grad,0)
         
-        self.grad = torch.cat((self.vp_grad,self.vs_grad,self.rho_grad),dim=0)
+        ########self.grad = torch.cat((self.vp_grad,self.vs_grad,self.rho_grad),dim=0)
+        self.grad = torch.cat((self.vp_grad,self.vs_grad),dim=0)
         #self.grad = self.vp_grad
         self.grad = torch.unsqueeze(self.grad,0)
         
