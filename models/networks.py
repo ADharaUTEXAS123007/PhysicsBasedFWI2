8e91113fe651a0b3e577c5b68dd726ca290b6fdc
Arnab Dhara <adhara@utexas.edu>
2022-05-17 14:13:48 -0500

    grad_obj_psv

--- models/networks.py
+++ models/networks.py
@@ -6966,8 +6966,8 @@ class AutoElMarmousiMar22_Net(nn.Module):
         #vs1    = minvs + vs1*(maxvs-minvs)
         #rho1   = minrho + rho1*(maxrho-minrho)
         
-        #########vp1    = torch.clip(vp1, min=minvp, max=maxvp)
-        #########vs1    = torch.clip(vs1, min=minvs, max=maxvs)
+        vp1    = torch.clip(vp1, min=minvp, max=maxvp)
+        vs1    = torch.clip(vs1, min=88.1, max=maxvs)
         #rho1   = torch.clip(rho1, min=1.719, max=maxrho*1)
         #######vp1 = minvp + vp1*(maxvp-minvp)
         ########vs1 = minvs + vs1*(maxvs-minvs)
