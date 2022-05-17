42a59fc252df4145287903c29f991d7db3d4eadd
Arnab Dhara <adhara@utexas.edu>
2022-05-17 04:23:17 -0500

    grad_obj_psv

--- trainVelAutoElMar22ModelPhy.sh
+++ trainVelAutoElMar22ModelPhy.sh
@@ -1,4 +1,4 @@
 rm -rf ./marmousiEl9Mar
 mkdir ./marmousiEl9Mar
 rm -rf /disk/student/adhara/Fall2021/PhysicsBasedFWI/checkpoints/MarmousiElMar22/*.txt
-no_proxy=localhost python trainValLatent4dVel2Elastic.py --dataroot /disk/student/adhara/Fall2021/FCNVMB-Deep-learning-based-seismic-velocity-model-building/MarmousiEl/ --name MarmousiElMar22 --model AutoElMar22 --direction AtoB --input_nc 35 --output_nc 1 --display_port 9998 --n_epochs 4000 --n_epochs_decay 500 --batch_size 8 --gpu_ids 1 --no_html --display_freq 1 --print_freq 1 --lr 0.005 --verbose --save_epoch_freq 10 --init_type normal
+no_proxy=localhost python trainValLatent4dVel2Elastic.py --dataroot /disk/student/adhara/Fall2021/FCNVMB-Deep-learning-based-seismic-velocity-model-building/MarmousiEl/ --name MarmousiElMar22 --model AutoElMar22 --direction AtoB --input_nc 35 --output_nc 1 --display_port 9998 --n_epochs 4000 --n_epochs_decay 500 --batch_size 8 --gpu_ids 1 --no_html --display_freq 1 --print_freq 1 --lr 0.05 --verbose --save_epoch_freq 10 --init_type normal
