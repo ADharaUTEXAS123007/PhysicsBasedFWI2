83b64612bd3c242706643fa33ac8ca43e5320a2a
Arnab Dhara <adhara@utexas.edu>
2022-05-17 17:13:41 -0500

    grad_obj_psv

--- models/networks.py
+++ models/networks.py
@@ -9844,7 +9844,7 @@ class AutoMarmousi23_Net(nn.Module):
         #print("device ordinal :", self.devicek)
         source_amplitudes_true = source_amplitudes_true.to(devicek)
         #lstart = -1
-        num_batches = 3
+        num_batches = 2
         num_epochs = 1
         if (epoch1 > lstart):
             num_epochs = 1
