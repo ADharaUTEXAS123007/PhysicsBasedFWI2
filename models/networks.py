8e81c186d0a90c71d951a26f3d8549d760e61139
Arnab Dhara <adhara@utexas.edu>
2022-05-28 18:54:53 -0500

    nnfwi

--- models/networks.py
+++ models/networks.py
@@ -6958,8 +6958,8 @@ class AutoElMarmousiMar22_Net(nn.Module):
         #rho1[:,:,0:15,:] = 0
         #rho1 = self.final3(rho1)
         
-        vp1    = torch.unsqueeze(lowf[:,0,:,:],1) + vp1
-        vs1    = torch.unsqueeze(lowf[:,1,:,:],1) + vs1
+        #vp1    = torch.unsqueeze(lowf[:,0,:,:],1) + vp1
+        #vs1    = torch.unsqueeze(lowf[:,1,:,:],1) + vs1
         rho1   = torch.unsqueeze(lowf[:,2,:,:],1)*1 
         #########rho1   = torch.unsqueeze(lowf[:,2,:,:],1)
         
