9765260f8f14f26c962ec72e8dac1bd29d3f2d0c
Arnab Dhara <adhara@utexas.edu>
2022-06-04 03:57:50 -0500

    lnorm

--- models/networks.py
+++ models/networks.py
@@ -6977,7 +6977,7 @@ class AutoElMarmousiMar22_Net(nn.Module):
         #vs1  = minvs + vs1f*(maxvs-minvs)
         
         vp1    = torch.clip(vp1, min=minvp, max=maxvp)
-        vs1    = torch.clip(vs1, min=881.0, max=maxvs)
+        vs1    = torch.clip(vs1, min=8.810, max=maxvs)
         ####rho1   = torch.clip(rho1, min=17.199993, max=maxrho)
         #######vp1 = minvp + vp1*(maxvp-minvp)
         ########vs1 = minvs + vs1*(maxvs-minvs)
