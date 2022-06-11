496cca6939cb4304e7adb5047bf3694b6a74ca57
Arnab Dhara <adhara@utexas.edu>
2022-06-11 16:58:21 -0500

    10

--- models/networks.py
+++ models/networks.py
@@ -7334,7 +7334,7 @@ class AutoElMarmousiMar22_Net(nn.Module):
         #for i, freq in enumerate([20]
         #d.add_fwi_stage(fc_low=0.0, fc_high=int(epoch1/10)+1.0)
         #d.add_fwi_stage(fc_low=0.0, fc_high=30.0)
-        d.add_fwi_stage(fc_low=3.0, fc_high=freq, lnorm=1)
+        d.add_fwi_stage(fc_low=3.0, fc_high=freq)
         # if ((epoch1 >= 0) and (epoch1 <=100 )):
         #     d.add_fwi_stage(fc_low=0.0, fc_high=2.0)
         # #     #print(f'Stage {i+1}:\n\t{d.fwi_stages[i]}\n')
