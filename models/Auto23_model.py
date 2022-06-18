5807a5143c9be2b404d4df8f5de506264c6977d2
Arnab Dhara <adhara@utexas.edu>
2022-06-18 10:57:17 -0500

    grad_obj_psv

--- models/Auto23_model.py
+++ models/Auto23_model.py
@@ -328,7 +328,7 @@ class Auto23Model(BaseModel):
         
         #print("shape of fake_B :", np.shape(self.fake_B))
         #print("shape of grad :", np.shape(self.grad))   
-        if (epoch1 % 3 == 0): 
+        if (epoch1 % 1 == 0): 
             filen = './marmousi23/Grad3AD' + str(batch)+'ep'+str(epoch1)+'.npy' #switch on for physics based fwi       
             np.save(filen, self.grad.cpu().detach().numpy())  #switch on physics based fwi
         
