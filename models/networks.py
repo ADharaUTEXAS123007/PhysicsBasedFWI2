2e43542a337dee45b1142383bb66a714ba9a18a7
Arnab Dhara <adhara@utexas.edu>
2022-06-19 13:14:47 -0500

    fwi

--- models/networks.py
+++ models/networks.py
@@ -7698,8 +7698,8 @@ class AutoElFullMarmousiMar22_Net(nn.Module):
         #vs1    = minvs + vs1f*(maxvs-27.8)
         #vp1 =  minvp + vp1f*(maxvp - minvp)
         #vs1 = 88.10 + vs1f*(maxvs - 88.10)
-        #vp1    = torch.unsqueeze(lowf[:,0,:,:],1) + vp1f
-        #vs1    = torch.unsqueeze(lowf[:,1,:,:],1) + vs1f
+        vp1    = torch.unsqueeze(lowf[:,0,:,:],1) + vp1f
+        vs1    = torch.unsqueeze(lowf[:,1,:,:],1) + vs1f
         rho1   = torch.unsqueeze(lowf[:,2,:,:],1)
         #########rho1   = torch.unsqueeze(lowf[:,2,:,:],1)
 
