52d9d63e57efe2f9a2df4f66849cb2d5a9057465
Arnab Dhara <adhara@utexas.edu>
2022-07-16 13:08:55 -0500

    seam

--- models/networks.py
+++ models/networks.py
@@ -8807,11 +8807,11 @@ class AutoSEAMMar22_Net(nn.Module):
         vs_grad = 1.0*vs_grad*r2
         #vs_grad = vs_grad*0
         
-        r3 = np.max(rhost)/np.max(rho_grad)
-        rho_grad = torch.from_numpy(rho_grad.copy())
-        rho_grad = rho_grad.float()
+        ###r3 = np.max(rhost)/np.max(rho_grad)
+        ####rho_grad = torch.from_numpy(rho_grad.copy())
+        #####rho_grad = rho_grad.float()
         #r3 = 1.0
-        rho_grad = 1.0*rho_grad*r3*0.1
+        #####rho_grad = 1.0*rho_grad*r3*0.1
         
         filen = './marmousiSEAM9July/vpp' + str(epoch1) + '.npy' #switch on for physics based fwi       
         np.save(filen, vp_grad)  #switch on physics based fwi
