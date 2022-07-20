f438c5cd4948e11a29735c619ded6de3b463f0a9
Arnab Dhara <adhara@utexas.edu>
2022-07-19 22:09:09 -0500

    seam

--- models/networks.py
+++ models/networks.py
@@ -8352,8 +8352,8 @@ class AutoSEAMMar22_Net(nn.Module):
         #vs1     = f12
         #rho1    = f13
         
-        vp1f    = self.final1(vp1f)
-        vs1f    = self.final2(vs1f)
+        #################vp1f    = self.final1(vp1f)
+        ##################vs1f    = self.final2(vs1f)
         ############rho1   = self.final3(rho1)
         #print("shape of vp1 :", np.shape(vp1))
         #vp1[:,:,0:15,:] = 0
@@ -8367,12 +8367,12 @@ class AutoSEAMMar22_Net(nn.Module):
         print("minvp :", minvp)
         print("maxvs :", maxvs)
         print("minvs :", minvs)
-        vp1    = 15.0 + vp1f*(maxvp-15.0)
-        vs1    = 1.0 + vs1f*(maxvs-1.0)
+        #vp1    = 15.0 + vp1f*(maxvp-15.0)
+        #vs1    = 1.0 + vs1f*(maxvs-1.0)
         #vp1 =  minvp + vp1f*(maxvp - minvp)
         #vs1 = 88.10 + vs1f*(maxvs - 88.10)
-        ###################################vp1    = torch.unsqueeze(lowf[:,0,:,:],1) + vp1f
-        #################################vs1    = torch.unsqueeze(lowf[:,1,:,:],1) + vs1f
+        vp1    = torch.unsqueeze(lowf[:,0,:,:],1) + vp1f
+        vs1    = torch.unsqueeze(lowf[:,1,:,:],1) + vs1f
         ###############################rho1   = torch.unsqueeze(lowf[:,2,:,:],1)
         rho1   = torch.unsqueeze(lowf[:,2,:,:],1)
 
@@ -8383,8 +8383,8 @@ class AutoSEAMMar22_Net(nn.Module):
         #vp1  = minvp + vp1f*(maxvp-minvp)
         #vs1  = minvs + vs1f*(maxvs-minvs)
         
-        #####################################vp1    = torch.clip(vp1, min=minvp, max=maxvp)
-        ##########################################vs1    = torch.clip(vs1, min=minvs, max=maxvs)
+        vp1    = torch.clip(vp1, min=15.0, max=maxvp)
+        vs1    = torch.clip(vs1, min=1.0, max=maxvs)
         ####rho1   = torch.clip(rho1, min=17.199993, max=maxrho)
         #######vp1 = minvp + vp1*(maxvp-minvp)
         ########vs1 = minvs + vs1*(maxvs-minvs)
