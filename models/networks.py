4cdc659eb393fce6c4ff0790ba6128f4ad968c1b
Arnab Dhara <adhara@utexas.edu>
2022-07-31 07:25:10 -0500

    autoelmar

--- models/networks.py
+++ models/networks.py
@@ -8389,7 +8389,7 @@ class AutoElFullRhoMarmousiMar22_Net(nn.Module):
         
         vp1    = torch.clip(vp1, min=minvp, max=maxvp)
         vs1    = torch.clip(vs1, min=88.1, max=maxvs)
-        rho1   = torch.clip(rho1, min=171.9, max=maxrho)
+        rho1   = torch.clip(rho1, min=1719.0, max=maxrho)
         #######vp1 = minvp + vp1*(maxvp-minvp)
         ########vs1 = minvs + vs1*(maxvs-minvs)
         ##########vs1 = 8.810*torch.ones((vs10.size())).cuda(vs10.get_device())
