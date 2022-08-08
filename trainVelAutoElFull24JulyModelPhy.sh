1b45b59a475e2d02855398e44fbd959b3619720c
Arnab Dhara <adhara@utexas.edu>
2022-08-08 01:55:26 -0500

    seam

--- trainVelAutoElFull24JulyModelPhy.sh
+++ trainVelAutoElFull24JulyModelPhy.sh
@@ -1,4 +1,4 @@
 rm -rf ./marmousiEl24July
 mkdir ./marmousiEl24July
 rm -rf /disk/student/adhara/Fall2021/PhysicsBasedFWI/checkpoints/MarmousiElFull24July/*.txt
-no_proxy=localhost python trainValLatent4dVel2Elastic.py --dataroot /disk/student/adhara/Fall2021/FCNVMB-Deep-learning-based-seismic-velocity-model-building/MarmousiElLinRho/ --name MarmousiElFull24July --model AutoElFullRhoMar22 --direction AtoB --input_nc 45 --output_nc 1 --display_port 9998 --n_epochs 4000  --n_epochs_decay 2000 --batch_size 8 --gpu_ids 1 --no_html --display_freq 1 --print_freq 1 --lr 0.005 --verbose --save_epoch_freq 30 --init_type normal
+no_proxy=localhost python trainValLatent4dVel2Elastic.py --dataroot /disk/student/adhara/Fall2021/FCNVMB-Deep-learning-based-seismic-velocity-model-building/MarmousiElLinRho/ --name MarmousiElFull24July --model AutoElFullRhoMar22 --direction AtoB --input_nc 45 --output_nc 1 --display_port 9998 --n_epochs 4000  --n_epochs_decay 2000 --batch_size 8 --gpu_ids 1 --no_html --display_freq 1 --print_freq 1 --lr 0.0025 --verbose --save_epoch_freq 30 --init_type normal
