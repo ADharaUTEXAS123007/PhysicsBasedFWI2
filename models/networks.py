7806b3cba93512cac009f915f69c64ebde0d34eb
Arnab Dhara <adhara@utexas.edu>
2022-08-17 06:53:19 -0500

    seam

--- models/networks.py
+++ models/networks.py
@@ -9376,7 +9376,7 @@ class AutoSEAMMar22_Net(nn.Module):
         d.RHOUPPERLIM = 1000.0
         d.RHOLOWERLIM = 1000.0
         d.SWS_TAPER_GRAD_HOR = 1
-        d.EXP_TAPER_GRAD_HOR = 1.0
+        d.EXP_TAPER_GRAD_HOR = 2.0
         #d.forward(model, src, rec)
         #os.system('mpirun -np 4 hello')
         filen = './marmousiSEAM9July/vpmod' + str(epoch1) + '.npy' #switch on for physics based fwi         
