278a487dde1109c8bdb386aeaa4c34484ae3a32e
Arnab Dhara <adhara@utexas.edu>
2022-08-24 00:28:10 -0500

    seam

--- models/networks.py
+++ models/networks.py
@@ -9042,8 +9042,8 @@ class AutoSEAMMar22_Net(nn.Module):
         #vs1     = f12
         #rho1    = f13
         
-        vp1f    = self.final1(vp1f)
-        vs1f    = self.final2(vs1f)
+        #vp1f    = self.final1(vp1f)
+        #vs1f    = self.final2(vs1f)
         ############rho1   = self.final3(rho1)
         #print("shape of vp1 :", np.shape(vp1))
         #vp1[:,:,0:15,:] = 0
@@ -9061,8 +9061,8 @@ class AutoSEAMMar22_Net(nn.Module):
         #vs1    = 1.0 + vs1f*(maxvs-1.0)
         #vp1 =  minvp + vp1f*(maxvp - minvp)
         #vs1 = 88.10 + vs1f*(maxvs - 88.10)
-        ########vp1    = torch.unsqueeze(lowf[:,0,:,:],1) + vp1f
-        #########vs1    = torch.unsqueeze(lowf[:,1,:,:],1) + vs1f
+        vp1    = torch.unsqueeze(lowf[:,0,:,:],1) + vp1f
+        vs1    = torch.unsqueeze(lowf[:,1,:,:],1) + vs1f
         ###############################rho1   = torch.unsqueeze(lowf[:,2,:,:],1)
         rho1   = torch.unsqueeze(lowf[:,2,:,:],1)
 
@@ -9073,10 +9073,10 @@ class AutoSEAMMar22_Net(nn.Module):
         #vp1  = minvp + vp1f*(maxvp-minvp)
         #vs1  = minvs + vs1f*(maxvs-minvs)
         
-        ##############vp1    = torch.clip(vp1, min=minvp, max=maxvp)
-        #################vs1    = torch.clip(vs1, min=1.330, max=maxvs)
-        vp1 = minvp + vp1f*(maxvp-minvp)
-        vs1  = 1.330 + vs1f*(maxvs-1.330)
+        vp1    = torch.clip(vp1, min=minvp, max=maxvp)
+        vs1    = torch.clip(vs1, min=1.330, max=maxvs)
+        ####vp1 = minvp + vp1f*(maxvp-minvp)
+        ####vs1  = 1.330 + vs1f*(maxvs-1.330)
         ####rho1   = torch.clip(rho1, min=17.199993, max=maxrho)
         #######vp1 = minvp + vp1*(maxvp-minvp)
         ########vs1 = minvs + vs1*(maxvs-minvs)
@@ -9378,7 +9378,7 @@ class AutoSEAMMar22_Net(nn.Module):
         d.RHOUPPERLIM = 1000.0
         d.RHOLOWERLIM = 1000.0
         d.SWS_TAPER_GRAD_HOR = 1
-        d.EXP_TAPER_GRAD_HOR = 3.0
+        d.EXP_TAPER_GRAD_HOR = 3.5
         #d.forward(model, src, rec)
         #os.system('mpirun -np 4 hello')
         filen = './marmousiSEAM9July/vpmod' + str(epoch1) + '.npy' #switch on for physics based fwi         
