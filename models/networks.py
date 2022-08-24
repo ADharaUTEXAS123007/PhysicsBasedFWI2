d0a28f57c736431d6bffb460bfd65e896775c0da
Arnab Dhara <adhara@utexas.edu>
2022-08-24 13:41:37 -0500

    po

--- models/networks.py
+++ models/networks.py
@@ -8856,7 +8856,7 @@ class AutoSEAMMar22_Net(nn.Module):
         self.decoder_input1 = nn.Linear(filters[3]*63*25, latent_dim) #for marmousi 101x101
         #self.decoder_input = nn.Linear(latent_dim, filters[3]*100*26) #for marmousi 101x101
         #self.decoder_input1 = nn.Linear(filters[1]*100*18, latent_dim) #for marmousi 101x101
-        self.decoder_input = nn.Linear(latent_dim, filters[3]*26*12) #for marmousi 101x101
+        self.decoder_input = nn.Linear(latent_dim, filters[4]*26*12) #for marmousi 101x101
         #self.decoder_inputRho = nn.Linear(latent_dim, 1*300*100)
         
         self.up41 = autoUp5(filters[4], filters[3], self.is_deconv)
@@ -8910,9 +8910,9 @@ class AutoSEAMMar22_Net(nn.Module):
     def forward(self, inputs1, inputs2, lstart, epoch1, latentI, lowf, inputs3, freq, idx, it):
         #filters = [16, 32, 64, 128, 256]
         #filters = [2, 4, 8, 16, 32]
-        filters = [32, 64, 128, 256, 512]
+        #filters = [32, 64, 128, 256, 512]
         #filters = [4,8,16,32,64]
-        #filters = [8, 16, 32, 64, 128]  ###this works very well
+        filters = [8, 16, 32, 64, 128]  ###this works very well
         #filters = [1, 1, 2, 4, 16]
         #filters = [16, 32, 64, 128, 256]
         #filters = [4, 8, 16, 32, 64]
