f6f8cf066cb0cb11558ed41b70f783eaa303b44e
Arnab Dhara <adhara@utexas.edu>
2022-08-26 07:34:21 -0500

    d

--- models/networks.py
+++ models/networks.py
@@ -8856,7 +8856,7 @@ class AutoSEAMMar22_Net(nn.Module):
         self.decoder_input1 = nn.Linear(filters[3]*63*42, latent_dim) #for marmousi 101x101
         #self.decoder_input = nn.Linear(latent_dim, filters[3]*100*26) #for marmousi 101x101
         #self.decoder_input1 = nn.Linear(filters[1]*100*18, latent_dim) #for marmousi 101x101
-        self.decoder_input = nn.Linear(latent_dim, filters[3]*60*80) #for marmousi 101x101
+        self.decoder_input = nn.Linear(latent_dim, filters[3]*65*85) #for marmousi 101x101
         #self.decoder_inputRho = nn.Linear(latent_dim, 1*300*100)
         
         #self.up41 = autoUp5(filters[4], filters[3], self.is_deconv)
@@ -8988,7 +8988,7 @@ class AutoSEAMMar22_Net(nn.Module):
         #####z = inputs2
         #z = z.view(-1, filters[3], 250, 51) #for marmousi model
         #print("shape of z :", np.shape(z))
-        z = z.view(-1, filters[3], 60, 80)
+        z = z.view(-1, filters[3], 65, 85)
         #zrho = zrho.view(-1, 1, 100, 300)
         
         #up41    = self.up41(z)
