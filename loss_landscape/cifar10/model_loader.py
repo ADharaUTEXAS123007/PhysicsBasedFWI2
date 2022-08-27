eace010cb23cd80f0e87a6abbf15685a8590102e
Arnab Dhara <adhara@utexas.edu>
2022-08-27 15:49:27 -0500

    d

--- loss_landscape/cifar10/model_loader.py
+++ loss_landscape/cifar10/model_loader.py
@@ -3,6 +3,7 @@ import torch, torchvision
 import cifar10.models.vgg as vgg
 import cifar10.models.resnet as resnet
 import cifar10.models.densenet as densenet
+import cifar10.models.adjointnet as adjointnet
 
 # map between model name and function
 models = {
@@ -35,6 +36,7 @@ models = {
     'wrn56_8_noshort'       : resnet.WRN56_8_noshort,
     'wrn110_2_noshort'      : resnet.WRN110_2_noshort,
     'wrn110_4_noshort'      : resnet.WRN110_4_noshort,
+    'adjointnet'            : adjointnet.ADJOINTNET
 }
 
 def load(model_name, model_file=None, data_parallel=False):
