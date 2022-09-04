6d02bea19cab179892875f843ef152d79a4149a0
Arnab Dhara <adhara@utexas.edu>
2022-09-04 15:47:38 -0500

    vpmodSEG

--- models/networks.py
+++ models/networks.py
@@ -9299,7 +9299,7 @@ class AutoSEAMMar22_Net(nn.Module):
         #tshots = 8
         ###xsrc = xsrcoriginal[idx[it::1]]
         ############################xsrc = xsrcoriginal[idx[0:14]]
-        xsrc = xsrcoriginal[idx[0:6]]
+        xsrc = xsrcoriginal[idx[0:4]]
         #xsrc = xsrcoriginal
         #print("xsrc1 :", xsrc)
         #xsrc = np.sort(xsrc)
