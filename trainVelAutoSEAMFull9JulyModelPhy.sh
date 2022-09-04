7598b9d850a476900e5a6fee86c269390e7967c3
Arnab Dhara <adhara@utexas.edu>
2022-09-04 10:46:16 -0500

    simple24

--- trainVelAutoSEAMFull9JulyModelPhy.sh
+++ trainVelAutoSEAMFull9JulyModelPhy.sh
@@ -1,4 +1,4 @@
 rm -rf ./marmousiSEAM9July
 mkdir ./marmousiSEAM9July
 rm -rf /disk/student/adhara/Fall2021/PhysicsBasedFWI/checkpoints/MarmousSEAM9July/*.txt
-no_proxy=localhost python trainValLatent4dVel2Elastic.py --dataroot /disk/student/adhara/Fall2021/FCNVMB-Deep-learning-based-seismic-velocity-model-building/SEAM/ --name marmousiSEAM9July --model AutoSEAMMar22 --direction AtoB --input_nc 39 --output_nc 1 --display_port 9998 --n_epochs 4000  --n_epochs_decay 2500 --batch_size 8 --gpu_ids 7 --no_html --display_freq 1 --print_freq 1 --lr 0.005--verbose --save_epoch_freq 30 --init_type xavier
+no_proxy=localhost python trainValLatent4dVel2Elastic.py --dataroot /disk/student/adhara/Fall2021/FCNVMB-Deep-learning-based-seismic-velocity-model-building/SEAM/ --name marmousiSEAM9July --model AutoSEAMMar22 --direction AtoB --input_nc 39 --output_nc 1 --display_port 9998 --n_epochs 4000  --n_epochs_decay 2500 --batch_size 8 --gpu_ids 7 --no_html --display_freq 1 --print_freq 1 --lr 0.005 --verbose --save_epoch_freq 30 --init_type xavier
