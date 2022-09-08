d3879d211f7abbfcff6e96c9955453883747ecfe
Arnab Dhara <adhara@utexas.edu>
2022-09-08 02:56:30 -0500

    vpmodSEG

--- models/networks.py
+++ models/networks.py
@@ -8855,7 +8855,7 @@ class AutoSEAMMar22_Net(nn.Module):
         ##self.decoder_input1 = nn.Linear(filters[1]*250*51, latent_dim) #for marmousi 151x200
         #self.decoder_input1 = nn.Linear(filters[2]*125*26, latent_dim) #for marmousi 151x200
         #self.decoder_input = nn.Linear(latent_dim, filters[2]*500*102) #for marmousi 151x200
-        self.decoder_input1 = nn.Linear(filters[3]*63*18, latent_dim) #for marmousi 101x101
+        self.decoder_input1 = nn.Linear(filters[3]*63*20, latent_dim) #for marmousi 101x101
         #self.decoder_input = nn.Linear(latent_dim, filters[3]*100*26) #for marmousi 101x101
         #self.decoder_input1 = nn.Linear(filters[1]*100*18, latent_dim) #for marmousi 101x101
         self.decoder_input = nn.Linear(latent_dim, filters[3]*13*23) #for marmousi 101x101
