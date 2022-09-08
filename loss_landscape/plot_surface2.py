551292a24bf2c9c287c1d5cc9b98bdbf03c686d8
Arnab Dhara <adhara@utexas.edu>
2022-09-08 09:33:36 -0500

    vpmodSEG

--- loss_landscape/plot_surface2.py
+++ loss_landscape/plot_surface2.py
@@ -186,6 +186,28 @@ def crunch2(surf_file, net, w, s, d, loss_key, acc_key, comm, rank, args):
     # stored in 'd') are stored in 'coords'.
     inds, coords, inds_nums = scheduler.get_job_indices(losses, xcoordinates, ycoordinates, comm)
     print("inds :", inds)
+    
+    # Loop over all uncalculated loss values
+    for count, ind in enumerate(inds):
+        # Get the coordinates of the loss value being calculated
+        coord = coords[count]
+
+        # Load the weights corresponding to those coordinates into the net
+        if args.dir_type == 'weights':
+            net_plotter.set_weights(net.module if args.ngpu > 1 else net, w, d, coord)
+        elif args.dir_type == 'states':
+            net_plotter.set_states(net.module if args.ngpu > 1 else net, s, d, coord)
+
+        # Record the time to compute the loss value
+        loss_start = time.time()
+        #loss, acc = evaluation.eval_loss(net, criterion, dataloader, args.cuda)
+        loss = 10
+        acc = 10
+        loss_compute_time = time.time() - loss_start
+
+        # Record the result in the local array
+        losses.ravel()[ind] = loss
+        accuracies.ravel()[ind] = acc
 
     f.close()
 
