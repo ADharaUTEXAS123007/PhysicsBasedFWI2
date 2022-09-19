1d8112a17685d311653fdc75e7a730a8e8477898
Arnab Dhara <adhara@wireless-10-148-127-134.public.utexas.edu>
2022-09-19 16:30:18 -0500

    swapaxes

--- loss_landscape/loss3.sh
+++ loss_landscape/loss3.sh
@@ -1,2 +1,2 @@
 #mpirun -np 36  /disk/student/adhara/WORK/DeniseFWI/virginFWI/DENISE-Black-Edition/bin/denise  ./LOSS_CURVE_DATA/seis.inp ./LOSS_CURVE_DATA/seis_fwi.inp
-python plot_surface2.py --cuda --model elasticnet --x=-1:1:20 --y=-1:1:20 --model_file /disk/student/adhara/Fall2021/PhysicsBasedFWI/checkpoints/MarmousiEl22/500_net_G.pth --dir_type weights --xnorm filter --xignore biasbn --ynorm filter --yignore biasbn  --surf_file elastic4215.h5 --plot
+python plot_surface2.py --cuda --model elasticnet --x=-1:1:20 --y=-1:1:20 --model_file /disk/student/adhara/Fall2021/PhysicsBasedFWI/checkpoints/MarmousiEl22/500_net_G.pth --dir_type weights --xnorm filter --xignore biasbn --ynorm filter --yignore biasbn  --surf_file elastic19Sep.h5 --plot
