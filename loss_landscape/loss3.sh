ee8116e546250783bf54f60809e84f3131a24f2e
Arnab Dhara <adhara@wireless-10-148-48-24.public.utexas.edu>
2022-09-21 04:39:51 -0500

    swapaxes

--- loss_landscape/loss3.sh
+++ loss_landscape/loss3.sh
@@ -1,3 +1,3 @@
 #mpirun -np 36  /disk/student/adhara/WORK/DeniseFWI/virginFWI/DENISE-Black-Edition/bin/denise  ./LOSS_CURVE_DATA/seis.inp ./LOSS_CURVE_DATA/seis_fwi.inp
-python plot_surface2.py --cuda --model elasticnet --x=-1:1:20 --y=-1:1:20 --model_file /disk/student/adhara/Fall2021/PhysicsBasedFWI/checkpoints/MarmousiEl22/10_net_G.pth --dir_type weights --xnorm filter --xignore biasbn --ynorm filter --yignore biasbn  --surf_file elastic19Sep10.h5 --plot
+python plot_surface2.py --cuda --model elasticnet --x=-1:1:20 --y=-1:1:20 --model_file /disk/student/adhara/Fall2021/PhysicsBasedFWI/checkpoints/MarmousiEl22/350_net_G.pth --dir_type weights --xnorm filter --xignore biasbn --ynorm filter --yignore biasbn  --surf_file elastic19Sep350.h5 --plot
 #plot_2D.plot_2d_contour('elastic19Sep.h5','train_loss',2.65,20.69,0.4,True)
