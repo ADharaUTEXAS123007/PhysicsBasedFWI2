4db440248482d8b4721c6261f1a6934670db49c9
Arnab Dhara <adhara@wireless-10-148-40-52.public.utexas.edu>
2022-10-05 22:01:23 -0500

    seam

--- models/networks.py
+++ models/networks.py
@@ -9379,13 +9379,13 @@ class AutoSEAMMar22_Net(nn.Module):
         d.EXP_TAPER_GRAD_HOR = 2.0
         #d.forward(model, src, rec)
         #os.system('mpirun -np 4 hello')
-        filen = './marmousiSEAM9July/vpmodSEG' + str(epoch1) + '.npy' #switch on for physics based fwi         
+        filen = './marmousiSEAM9July/vpmodSEG5Oct' + str(epoch1) + '.npy' #switch on for physics based fwi         
         np.save(filen, vpst)  #switch on physics based fwi
         
-        filen = './marmousiSEAM9July/vsmodSEG' + str(epoch1) + '.npy' #switch on for physics based fwi     
+        filen = './marmousiSEAM9July/vsmodSEG5Oct' + str(epoch1) + '.npy' #switch on for physics based fwi     
         np.save(filen, vsst)  #switch on physics based fwi
         
-        filen = './marmousiSEAM9July/rhomodSEG' + str(epoch1) + '.npy' #switch on for physics based fwi     
+        filen = './marmousiSEAM9July/rhomodSEG5Oct' + str(epoch1) + '.npy' #switch on for physics based fwi     
         np.save(filen, rhost)  #switch on physics based fwi
         
         
@@ -9464,13 +9464,13 @@ class AutoSEAMMar22_Net(nn.Module):
         #####rho_grad = 1.0*rho_grad*r3*0.1
         rho_grad = 0*vs_grad
         
-        filen = './marmousiSEAM9July/vppS2EG' + str(epoch1) + '.npy' #switch on for physics based fwi       
+        filen = './marmousiSEAM9July/vppS2EG5Oct' + str(epoch1) + '.npy' #switch on for physics based fwi       
         np.save(filen, vp_grad)  #switch on physics based fwi
         
-        filen = './marmousiSEAM9July/vssS2EG' + str(epoch1) + '.npy' #switch on for physics based fwi       
+        filen = './marmousiSEAM9July/vssS2EG5Oct' + str(epoch1) + '.npy' #switch on for physics based fwi       
         np.save(filen, vs_grad)  #switch on physics based fwi
         
-        filen = './marmousiSEAM9July/rhooS2EG' + str(epoch1) + '.npy' #switch on for physics based fwi       
+        filen = './marmousiSEAM9July/rhooS2EG5Oct' + str(epoch1) + '.npy' #switch on for physics based fwi       
         np.save(filen, rho_grad)  #switch on physics based fwi
         
         print('grads names')
