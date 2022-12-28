38aa84c5c7a96f77977bd8f01c1e41a202228210
Arnab Dhara <adhara@IGA23087.local>
2022-12-28 09:49:56 -0600

    d

--- models/networks.py
+++ models/networks.py
@@ -9654,7 +9654,7 @@ class AutoRealData_Net(nn.Module):
         #####z = inputs2
         #z = z.view(-1, filters[3], 250, 51) #for marmousi model
         #print("shape of z :", np.shape(z))
-        z = z.view(-1, filters[3], 28*4, 130*4)
+        z = z.view(-1, filters[2], 28*4, 130*4)
         #zrho = zrho.view(-1, 1, 100, 300)
         
         ######up41    = self.up41(z)
