9c5341afe28b6c21f2663e30e533e875de81fb90
Arnab Dhara <adhara@IGA23087.local>
2022-12-29 11:56:29 -0600

    d

--- models/networks.py
+++ models/networks.py
@@ -9494,8 +9494,8 @@ class AutoRealData_Net(nn.Module):
         #filters = [16, 32, 64, 128, 256]
         #filters = [32, 64, 128, 256, 512]
         #filters = [4, 8, 16, 32, 64]
-        #filters = [2, 4, 8, 16, 32] #this works best result so far for marmousi model
-        filters = [8, 16, 32, 64, 128]
+        filters = [2, 4, 8, 16, 32] #this works best result so far for marmousi model
+        #filters = [8, 16, 32, 64, 128]
         #filters = [2, 4, 4, 8, 16]
         #filters = [8, 16, 32, 64, 128] 
         #filters = [2, 4, 8, 16, 32]
@@ -9579,8 +9579,8 @@ class AutoRealData_Net(nn.Module):
         #filters = [4, 8, 16, 32, 64]
         #filters = [32, 64, 128, 256, 512]
         #filters = [4,8,16,32,64]
-        filters = [8, 16, 32, 64, 128]  ###this works very well
-        #filters = [2, 2, 4, 8, 16]
+        #filters = [8, 16, 32, 64, 128]  ###this works very well
+        filters = [2, 4, 8, 16, 32]
         #filters = [16, 32, 64, 128, 256]
         #filters = [4, 8, 16, 32, 64]
         #filters = [2, 4, 8, 16, 32]
