b619e2d78ad09ddafd26b09ba1a6443734eb72f6
Arnab Dhara <adhara@IGA23087.local>
2023-01-05 01:39:21 -0600

    fwi

--- models/networks.py
+++ models/networks.py
@@ -12185,8 +12185,8 @@ class AutoMarmousi24_Net(nn.Module):
     # forward modeling to compute gradients
     def prop(self, inputs, vel, lstart, epoch1, mintrue, maxtrue, true):
         
-        torch.cuda.set_device(1)  #RB Necessary if device <> 0
-        GPU_string='cuda:'+str(1)
+        torch.cuda.set_device(4)  #RB Necessary if device <> 0
+        GPU_string='cuda:'+str(4)
         devicek = torch.device(GPU_string)
         #vel = vel.to(devicek)
         #net1out1 = mintrue + vel*(maxtrue-mintrue)
