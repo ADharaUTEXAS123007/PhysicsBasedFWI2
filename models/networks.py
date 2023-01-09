9332f7155d59d52c263b2da55c580b35663bc49d
Arnab Dhara <adhara@IGA23087.local>
2023-01-09 04:10:48 -0600

    d

--- models/networks.py
+++ models/networks.py
@@ -7948,7 +7948,7 @@ class AutoElFullMarmousiMar22_Net(nn.Module):
         d.NPROCX = 6
         d.NPROCY = 5
         d.PHYSICS = 1
-        d.FC_SPIKE_1 = 6.0
+        d.FC_SPIKE_1 = 5.0
         d.QUELLART = 6
         #d.FC_SPIKE_2 = 18.0
         #d.TIME = 6.0
