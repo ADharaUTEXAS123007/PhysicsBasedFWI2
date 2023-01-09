b54da8892ec7b69403dac0a1a9030b8ac35613e6
Arnab Dhara <adhara@IGA23087.local>
2023-01-09 13:24:11 -0600

    fwi

--- models/networks.py
+++ models/networks.py
@@ -12439,6 +12439,7 @@ class AutoMarmousiNF_Net(nn.Module):
         print("device :",next(self.model.parameters()).device)
         #print("device :", self.model.parameters().device)
         z = torch.randn(1,150,200)
+        z = z.to(next(self.model.parameters()).device)
         f1 = self.model(z)
 
         print("shape of f1 :", np.shape(f1))
