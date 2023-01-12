5e3545cb0779687473d6468b8166834a9305ddfc
Arnab Dhara <adhara@IGA23087.local>
2023-01-12 17:48:50 -0600

    fwi

--- trainVelAutoElFull4JanModelPhy.sh
+++ trainVelAutoElFull4JanModelPhy.sh
@@ -1,4 +1,4 @@
 #rm -rf ./marmousiEl4Jan
 #mkdir ./marmousiEl4Jan
 rm -rf /disk/student/adhara/Fall2021/PhysicsBasedFWI/checkpoints/MarmousiElFull4Jan/*.txt
-no_proxy=localhost python trainValLatent4dVel2Elastic.py --dataroot /disk/student/adhara/Fall2021/FCNVMB-Deep-learning-based-seismic-velocity-model-building/MarmousiElLinConst/ --name MarmousiElFull4Jan --model AutoElFullMar22 --direction AtoB --input_nc 45 --output_nc 1 --display_port 9998 --n_epochs 4000  --n_epochs_decay 2500 --batch_size 8 --gpu_ids 5 --no_html --display_freq 1 --print_freq 1 --lr 0.0025 --verbose --save_epoch_freq 20 --init_type normal
+no_proxy=localhost python trainValLatent4dVel2Elastic.py --dataroot /disk/student/adhara/Fall2021/FCNVMB-Deep-learning-based-seismic-velocity-model-building/MarmousiElLinConst/ --name MarmousiElFull4Jan --model AutoElFullMar22 --direction AtoB --input_nc 45 --output_nc 1 --display_port 9998 --n_epochs 4000  --n_epochs_decay 2500 --batch_size 8 --gpu_ids 5 --no_html --display_freq 1 --print_freq 1 --lr 0.0025 --verbose --save_epoch_freq 20 --init_type xavier
