7372e3e70b1db4f2f79618ce6586f4c8e82f3f8c
Arnab Dhara <adhara@IGA23087.local>
2023-01-14 09:26:02 -0600

    DT

--- models/networks.py
+++ models/networks.py
@@ -12443,7 +12443,7 @@ class AutoMarmousiNF_Net(nn.Module):
         #result = torch.flatten(f1, start_dim=1)
         #print(" shape of grad :", np.shape(grad))
 
-        return f1, grad, latent1, lossT, down3, up2, up1
+        return f2, grad, latent1, lossT, down3, up2, up1
     
     # Initialization of Parameters
     def  _initialize_weights(self):
