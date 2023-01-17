e18deda3318b21b583483241c9dc1c154e9275ed
Arnab Dhara <adhara@utexas.edu>
2023-01-16 19:49:04 -0600

    fwi

--- models/networks.py
+++ models/networks.py
@@ -12434,8 +12434,8 @@ class AutoMarmousiNF_Net(nn.Module):
         f2 = lowf + 0.001*f1
         f2 = torch.clip(f2,min=1500.0,max=4766.6)
 
-        #port = inputs1.repeat(10,1,1,1)
-        port = inputs1
+        port = inputs1.repeat(10,1,1,1)
+        #port = inputs1
         #print("shape of port :", np.shape(port))
 
         f2[(port==1500)] = 1500
@@ -12449,7 +12449,7 @@ class AutoMarmousiNF_Net(nn.Module):
         lossT = 0
         if (epoch1 > lstart):
             Grad = torch.empty_like(f2)
-            for i in range(1):
+            for i in range(10):
                 print("shape of i :", i)
                 [grad, lossT] = self.prop(inputs2, torch.unsqueeze(f2[i,:,:,:],0), lstart, epoch1, mintrue, maxtrue, inputs1)
                 grad = grad.to(inputs2.get_device())
