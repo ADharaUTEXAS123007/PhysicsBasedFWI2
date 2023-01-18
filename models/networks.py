e7e8510c7dd1c8ae1aa90982a4853e4e47bfd214
Arnab Dhara <adhara@utexas.edu>
2023-01-17 21:43:20 -0600

    fwi

--- models/networks.py
+++ models/networks.py
@@ -8246,7 +8246,7 @@ class AutoElFullRhoMarmousiMar22_Net(nn.Module):
         
         #print("shape of down4 :", np.shape(down4))
         
-        ####print("shape of down4 :", np.shape(down4))
+        print("shape of down4 :", np.shape(down4))
         result = torch.flatten(down4, start_dim=1)
         
         #####print("result shape :", np.shape(result))
