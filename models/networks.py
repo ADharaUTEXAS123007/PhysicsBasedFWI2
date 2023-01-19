e4dce90d897bec7f24a537fdf2d1800fa0f98ac8
Arnab Dhara <adhara@utexas.edu>
2023-01-19 14:57:46 -0600

    DT

--- models/networks.py
+++ models/networks.py
@@ -8730,7 +8730,7 @@ class AutoElFullRhoMarmousiMar22_Net(nn.Module):
 
         g1 = np.arange(np.shape(rho_grad)[0])
         g1 = g1**2
-        ss = np.repmat(g1,1,np.shape(rho_grad)[1])
+        ss = np.matlib.repmat(g1,1,np.shape(rho_grad)[1])
         #ss = g1.tile((np.shape(rho_grad)[1],1))
         print("shape of ss :", np.shape(ss))
         
