490be83c9d715e8845ceebe95526df4f4c0df0b3
Arnab Dhara <adhara@utexas.edu>
2023-01-24 05:38:00 -0600

    fwi

--- models/AutoWav_model.py
+++ models/AutoWav_model.py
@@ -141,7 +141,8 @@ class AutoWavModel(BaseModel):
         self.real_A = input['A' if AtoB else 'B'].to(self.device1)
         self.real_B = input['B' if AtoB else 'A'].to(self.device1)
         self.real_C = input['C'].to(self.device1)
-        #self.real_D = input['D'].to(self.device1)  
+        self.real_D = input['D'].to(self.device1)  
+        print("shape of real D :", np.shape(self.real_D))
         self.image_paths = input['A_paths' if AtoB else 'B_paths']
 
     def forward(self,epoch1,lstart):
