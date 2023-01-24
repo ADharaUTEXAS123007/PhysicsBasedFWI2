2a471dc7b20380125b2704a64ad04d377af3e336
Arnab Dhara <adhara@utexas.edu>
2023-01-24 10:02:33 -0600

    rand_wav

--- models/AutoWav_model.py
+++ models/AutoWav_model.py
@@ -154,7 +154,7 @@ class AutoWavModel(BaseModel):
         #netin1 = self.real_A[:, :, 1:800:2, :]
         if (epoch1 == 1):
             self.latent = torch.ones(1,1,1,1)
-        [self.fake_B,self.grad,self.latent,self.loss_D_MSE,self.down3,self.up2,self.up1] = self.netG(self.real_B,self.real_A,lstart,epoch1,self.latent,self.real_C,self.real_D,self.real_E,self.real_F)  # G(A)
+        [self.fake_B,self.grad,self.latent,self.loss_D_MSE,self.down3,self.up2,self.up1] = self.netG(self.real_B,self.real_A,lstart,epoch1,self.latent,self.real_C,self.real_D[0,:,:,:],self.real_E[0,:,:,:],self.real_F[0,:,:,:])  # G(A)
         #self.latent = self.latent.clone().detach()
         #print("self.latent :", self.latent)
         #self.real_C = self.fake_B
