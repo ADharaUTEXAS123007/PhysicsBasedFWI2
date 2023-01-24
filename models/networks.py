f3497cc4c413682e4676bd0c1971b593258ef601
Arnab Dhara <adhara@utexas.edu>
2023-01-24 08:52:23 -0600

    rand_wav

--- models/networks.py
+++ models/networks.py
@@ -12540,7 +12540,7 @@ class AutoMarmousiWav_Net(nn.Module):
         lossT = 0.0
 
         #inputwav = torch.randn(20,10,50).to(inputs1.get_device())
-        p1 = self.convWav1(rand_wav[0,:,:,:])
+        p1 = self.convWav1(rand_wav[0,:,:,0:500])
 
         print("shape of p1 :", np.shape(p1))
         p2 = self.maxWav1(p1)
