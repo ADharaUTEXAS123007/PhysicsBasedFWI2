98c8e050d24e17ddf0e25c1472e2a30f19f8f388
Arnab Dhara <adhara@utexas.edu>
2023-02-01 12:41:15 -0600

    dop

--- models/networks.py
+++ models/networks.py
@@ -8837,7 +8837,7 @@ class AutoElFullRhoMarmousiMar22_Net(nn.Module):
         rho_grad = np.flipud(rho_grad)
 
         g1 = np.arange(np.shape(rho_grad)[0])
-        g1 = g1**1.5
+        g1 = g1**2.0
         ss = rho_grad*0
         for i in range(np.shape(rho_grad)[1]):
              ss[:,i] = g1
