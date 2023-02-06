05133aad91b10f07bd419480795611624b014d94
Arnab Dhara <adhara@utexas.edu>
2023-02-05 19:03:17 -0600

    dop

--- models/AutoElFullRhoMar22_model.py
+++ models/AutoElFullRhoMar22_model.py
@@ -430,16 +430,16 @@ class AutoElFullRhoMar22Model(BaseModel):
             ############self.fake_B.backward(self.grad) #switch on for physics based fwi
             
             #################
-            #########555######## self.vp_grad = torch.unsqueeze(self.vp_grad,0)
-            ########555######### self.vp_grad = self.vp_grad.cuda(self.fake_Vp.get_device())
-            #######555########## self.fake_Vp.backward(self.vp_grad,retain_graph=True)
+            self.vp_grad = torch.unsqueeze(self.vp_grad,0)
+            self.vp_grad = self.vp_grad.cuda(self.fake_Vp.get_device())
+            self.fake_Vp.backward(self.vp_grad,retain_graph=True)
             #self.fake_Vs.retain_grad()
             #self.fake_Vp.retain_grad()
             #self.fake_Rho.retain_grad()
             
-            #########555######### self.vs_grad = torch.unsqueeze(self.vs_grad,0)
-            ########555######### self.vs_grad = self.vs_grad.cuda(self.fake_Vs.get_device())
-            ########555########## self.fake_Vs.backward(self.vs_grad,retain_graph=True)
+            self.vs_grad = torch.unsqueeze(self.vs_grad,0)
+            self.vs_grad = self.vs_grad.cuda(self.fake_Vs.get_device())
+            self.fake_Vs.backward(self.vs_grad,retain_graph=True)
             #self.fake_Vs.retain_grad()
             #self.fake_Vp.retain_grad()
             #self.fake_Rho.retain_grad()
@@ -448,9 +448,9 @@ class AutoElFullRhoMar22Model(BaseModel):
             #if (currenterror < 0.4*initerror):
                 #print("backpropagating density gradient")
             #if (currenterror < 0.5*initerror):
-            self.rho_grad = torch.unsqueeze(self.rho_grad,0)
-            self.rho_grad = self.rho_grad.cuda(self.fake_Rho.get_device())
-            self.fake_Rho.backward(self.rho_grad)
+            ###555#### self.rho_grad = torch.unsqueeze(self.rho_grad,0)
+            ###555### self.rho_grad = self.rho_grad.cuda(self.fake_Rho.get_device())
+            ######555### self.fake_Rho.backward(self.rho_grad)
                 
             #self.fake_Rho.retain_grad()
 
